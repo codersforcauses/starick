c980b5b206d7eb775f9662111ac8c51e9ef3e401
Subodh1911 <subodhkumar2285@gmail.com>
2023-11-26 08:38:13 +0530

    added footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,16 +5,43 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Footer: React.FC = () => {
     return (
-        <footer style={{ backgroundColor: 'skyblue', padding: '2rem', textAlign: 'center' }}>
-            <p style={{ color: 'white' }}>This is the footer component.</p>
-            <div style={{ marginTop: '2rem' }}>
-                <a href="https://www.facebook.com/StarickServices/" target="_blank" style={{ marginRight: '1rem' }}><FontAwesomeIcon icon={faFacebook} /></a>
-                <a href="https://twitter.com/starickceo" target="_blank" style={{ marginRight: '1rem' }}><FontAwesomeIcon icon={faTwitter} /></a>
-                <a href="https://www.linkedin.com/company/starick/" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a>
+        <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+            <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+                <div className="sm:flex sm:items-center sm:justify-between">
+                    <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Starick</span>
+                    </a>
+                    <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                        <li>
+                            <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                        </li>
+                        <li>
+                            <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+                        </li>
+                        <li>
+                            <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
+                        </li>
+                        <li>
+                            <a href="#" className="hover:underline">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+                <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+
+                <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Starick</a>. All Rights Reserved | <a href="www.example.com" className="hover:underline"> Privacy & Disclaimer </a> | Site by OM4</span>
+
+                <div className="container mx-auto my-2 flex justify-center">
+                    <a href="https://www.facebook.com/StarickServices/" target="_blank" className="mr-4"><FontAwesomeIcon icon={faFacebook} /></a>
+                    <a href="https://twitter.com/starickceo" target="_blank" className="mr-4"><FontAwesomeIcon icon={faTwitter} /></a>
+                    <a href="https://www.linkedin.com/company/starick/" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a>
+                </div>
+
             </div>
-            <p style={{ color: 'white', fontSize: '0.875rem', marginTop: '2rem' }}>©  2023 Starick. All Rights Reserved. | Privacy & Disclaimer | Site by OM4</p>
         </footer>
     );
 };
 
 export default Footer;
+
+
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,17 @@
 import Footer from "@/components/footer";
 import { Inter } from "next/font/google";
-// import '../styles/globals.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <h1>Hello!</h1>
-    </main>
+    <div>
+      <main
+        className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      >
+        <h1 className="bg-red-500">Hello!</h1>
+      </main>
+    <Footer/>
+    </div>
   );
 }
