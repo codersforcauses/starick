ba740499082ed664228f985b0877bf5436ec2be7
HuxleyBerry <huxley.berry@gmail.com>
2023-12-01 22:37:50 +0800

    make lists a component

--- src/pages/core-services.tsx
+++ src/pages/core-services.tsx
@@ -1,32 +1,38 @@
 import DropDown from '../components/dropdown'
 
-export default function CoreServices() {
-    const refugeSupportExamples = ["Emotional support", 
-                                    "Information and referral to relevant services", 
-                                    "Counselling",
-                                    "Case management",
-                                    "Conducting risk assessments",
-                                    "Safety plans"].map(text => <li className="list-disc list-inside">{text}</li>);
+function List({ items }: {items: String[]}) {
+    return (<ul>
+        {items.map((text, index) => <li className="list-disc list-inside" key={index}>{text}</li>)}
+        </ul>
+    );
+}
 
-    const childrenServicesExamples = ["Emotional support", 
-                                    "Referral to relevant services", 
-                                    "Counselling",
-                                    "Out-of-school activities and school holiday programs",
-                                    "Homework support",
-                                    "Art-related activities."].map(text => <li className="list-disc list-inside">{text}</li>)
+export default function CoreServices() {
+    const refugeSupportExamples = <List items={["Emotional support", 
+    "Information and referral to relevant services", 
+    "Counselling",
+    "Case management",
+    "Conducting risk assessments",
+    "Safety plans"]}/>
+    const childrenServicesExamples = <List items={["Emotional support", 
+    "Referral to relevant services", 
+    "Counselling",
+    "Out-of-school activities and school holiday programs",
+    "Homework support",
+    "Art-related activities"]}/>
 
     return (
         <>
             <div className="h-10 text-center bg-[--starick-green]">Navbar - replace with component later</div>
-            <div className="bg-neutral-200 flex flex-col justify-center items-center px-20">
+            <div className="bg-neutral-200 flex flex-col justify-center items-center px-40">
                 <p className="text-center text-2xl mt-1 font-semibold">Core Services</p>
                 <DropDown />
             </div>
             <img src={"images/starick-image1.jpg"}></img>
-            <div className="bg-neutral-200 flex flex-col justify-center items-center px-20">
+            <div className="bg-neutral-200 flex flex-col justify-center items-center px-40">
                 <p className="text-center text-2xl pt-1 font-semibold">Refuges</p>
                 <br></br>
-                <div className="flex flex-row items-center mb-4">
+                <div className="flex flex-row items-center mb-4 w-full">
                     <img src={"images/Icon-CRISIS-150x150.png"} className="w-24 h-24"></img>
                     <div className="pl-3">
                         <p>If you are in immediate physical danger, call the police now on 000. If you are not in immediate danger and would prefer to speak to a support worker at one of our refuges:</p>
@@ -42,8 +48,7 @@ export default function CoreServices() {
                     <p className="text-xl font-semibold">How we support the women in our refuges</p>
                     <br></br>
                     <p>Our refuge teams work hard to provide an integrated service, with the aim of increasing a client's sense of safety and wellbeing. The support provided at our refuges includes:</p>
-                    <br></br>
-                    <ul>{refugeSupportExamples}</ul>
+                    {refugeSupportExamples}
                     <br></br>
                     <p>We also offer our clients a range of other services and programs, to support their wellbeing.</p>
                     <br></br>
@@ -54,8 +59,7 @@ export default function CoreServices() {
                     <p>Starick has been at the forefront of recognising the impact of family and domestic violence on children and developing our services for children. Our focus on children continues through the work of our child advocates, who ensure that the supports and programs offered are effectively meeting the diverse needs of the children living at both refuges.</p>
                     <br></br>
                     <p>Our services for children currently include:</p>
-                    <br></br>
-                    <ul>{childrenServicesExamples}</ul>
+                    {childrenServicesExamples}
                     <br></br>
                     <p>Support from Starick continues after women and children leave our refuges through our outreach services.</p>
                     <br></br>
