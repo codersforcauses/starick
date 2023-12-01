d1a9e0ef5a9c14df6bbafba048e1aea9c0b04340
John Smith <makon1234@users.noreply.github.com>
2023-12-01 16:26:42 +0800

    remove useless code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,6 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <h1 className="bg-red-500">Hello!</h1>
     </main>
     </>
   );
