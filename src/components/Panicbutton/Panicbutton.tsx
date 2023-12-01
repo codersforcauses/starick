0511566ece845c0f9f34aa0892983953d5de3776
dimmerz92 <andrew.weymes@outlook.com>
2023-12-01 09:08:56 +0800

    refactor

--- src/components/Panicbutton/Panicbutton.tsx
+++ src/components/Panicbutton/Panicbutton.tsx
@@ -1,33 +1,29 @@
-import React, { useEffect, } from 'react';
-import { useRouter } from 'next/router';
-import Link from 'next/link';
+import React, { useEffect } from "react";
+import Link from "next/link";
 
 const Panicbutton = () => {
-    const router = useRouter();
+  useEffect(() => {
+    const handleKeyPress = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        event.preventDefault();
+        window.location.href = "https://www.google.com.au/";
+      }
+    };
 
-    useEffect(() => {
-        const handleKeyDown = (event: KeyboardEvent) => {
-          if (event.key === 'Escape') {
-            router.push('/Redirect');
-          }
-        };
+    window.addEventListener("keydown", handleKeyPress);
 
-        window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyPress);
+  }, []);
 
-        return () => {
-            window.removeEventListener('keydown', handleKeyDown);
-          };
-        }, []);
-
-    return (
-    <div className="fixed top-1/3 right-0">
-        <Link href="https://www.google.com">
-            <button className="px-4 py-2 bg-starick-slate text-white sqaure hover:bg-starick-orange">
-                <span className= "font-bold">X</span> Quick Exit (Esc)
-            </button>
-        </Link>
+  return (
+    <div className="fixed right-0 top-1/3">
+      <Link href="https://www.google.com.au/">
+        <button className="sqaure bg-[--starick-brown] px-4 py-2 text-white hover:bg-[--starick-orange]">
+          <span className="font-bold">X</span> Quick Exit (Esc)
+        </button>
+      </Link>
     </div>
-    );
+  );
 };
 
 export default Panicbutton;
