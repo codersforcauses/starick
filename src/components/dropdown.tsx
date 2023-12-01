14aeb76e507d645a8412a8615294123e27e41946
HuxleyBerry <huxley.berry@gmail.com>
2023-12-01 22:18:36 +0800

    start on core services page

---
+++ src/components/dropdown.tsx
@@ -0,0 +1,8 @@
+export default function DropDown() {
+  return (
+    <button className="w-full bg-[--starick-white] p-2 flex flex-row justify-between text-neutral-500 my-3">
+      <p>Page Navigation</p>
+      <p>▼</p>
+    </button>
+  )
+}
