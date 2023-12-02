0036480b1392c378a572fbc41be018d9af3e8a09
HuxleyBerry <huxley.berry@gmail.com>
2023-12-02 14:18:45 +0800

    change layout at top

--- src/components/core-services/services-section.tsx
+++ src/components/core-services/services-section.tsx
@@ -1,14 +1,18 @@
 import { ReactNode } from "react"
 
 interface SectionProps {
-    title: string;
+    title?: string;
     colour: string;
     sectionBody: ReactNode;
 }
 
+function Title() {
+
+}
+
 export default function Section({ title, colour, sectionBody }: SectionProps) {
-    return <div className={`${colour} md:px-40 px-6`}>
-        <p className="text-center text-2xl py-8 font-semibold">{title}</p>
+    return <div className={`${colour} md:px-40 px-6 py-2`}>
+        {title ? <p className="text-center text-2xl py-8 font-semibold">{title}</p> : null}
         { sectionBody }
     </div>
 }
--- src/pages/core-services.tsx
+++ src/pages/core-services.tsx
@@ -4,16 +4,16 @@ import RefugesSection from '../components/core-services/refuges'
 import PoliceSection from '../components/core-services/police-stations';
 import SafeAtHomeSection from '../components/core-services/safe-at-home';
 import TransitionalHousingSection from '../components/core-services/transitional-housing';
+import ServicesSection from "../components/core-services/services-section"
 
 export default function CoreServices() {
     return (
         <>
             <div className="h-10 text-center bg-[--starick-green]">Navbar - replace with component later</div>
-            <div className="bg-neutral-200 flex flex-col justify-center items-center px-40">
-                <p className="text-center text-2xl mt-1 font-semibold">Core Services</p>
-                <DropDown />
+            <div>
+            <img src={"images/starick-image1.jpg"} className="w-full" alt=""></img>
             </div>
-            <img src={"images/starick-image1.jpg"} className="h-40 w-full" alt=""></img>
+            <ServicesSection colour="bg-neutral-200" sectionBody={<DropDown />}/>
             <CounsellingSection />
             <RefugesSection />
             <TransitionalHousingSection />
