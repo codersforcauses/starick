9441a807a1cc3c899648afaf871ab56c9856ef70
John Smith <makon1234@users.noreply.github.com>
2023-12-02 13:18:01 +0800

    fix linting errors

--- src/components/panicbutton/panicbutton.tsx
+++ src/components/panicbutton/panicbutton.tsx
@@ -24,5 +24,4 @@ export default function PanicButton() {
       </Link>
     </div>
   );
-};
-
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,13 +7,12 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
-    <header>
-      <PanicButton />
-    </header>
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-    </main>
+      <header>
+        <PanicButton />
+      </header>
+      <main
+        className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      ></main>
     </>
   );
 }
