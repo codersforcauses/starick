85694af6d8cf76249fda0d2a062c61eecf42f0de
Andrew <andrew.weymes@outlook.com>
2023-12-04 22:51:23 +0800

    add start of fetch method from openai

---
+++ src/lib/chatbot/methods.ts
@@ -0,0 +1,25 @@
+import { GPTPayload } from "./validators";
+
+type GPTRes = {
+  success: boolean;
+  message: string | Response;
+};
+
+export async function fetchGPTResponse(payload: GPTPayload): Promise<GPTRes> {
+  const res = await fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`
+    },
+    body: JSON.stringify(payload)
+  });
+
+  if (!res.ok) {
+    return { success: false, message: "Unexpected Error" };
+  }
+
+  const data = await res.json();
+
+  return { success: false, message: data };
+}
