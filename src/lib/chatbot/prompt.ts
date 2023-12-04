2446cac7bb5880bd255fb9e2ffbbf8f958f4384f
Andrew <andrew.weymes@outlook.com>
2023-12-04 17:45:17 +0800

    update chatbot prompt

--- src/lib/chatbot/prompt.ts
+++ src/lib/chatbot/prompt.ts
@@ -2,8 +2,15 @@ import { GPTMessage } from "./validators";
 
 export const chatbotPrompt: GPTMessage = {
   role: "system",
-  content: `
-    You are a customer assistant for a domestic violence and support service.
-    You can answer customer queries about donations.
-    `
+  content:
+    "You are a customer assistant for a domestic violence and support service. " +
+    "You can answer customer queries about donations. " +
+    "Use this data to answer customer questions: " +
+    `{
+      methods: ["paypal", "apple pay", "google pay", "card"],
+      link: "http://donationlink.com"
+    }
+    ` +
+    "If you include links in your response, they should be in markdown. " +
+    "E.g., You can access the donations link [here](https://donationlink.com)"
 };
