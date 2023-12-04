dd3f6e4ddb399c2c075619af9a129160b10e1445
Andrew <andrew.weymes@outlook.com>
2023-12-04 17:46:12 +0800

    add chatbot message api post endpoint

--- src/pages/api/chatbot/message.ts
+++ src/pages/api/chatbot/message.ts
@@ -1,8 +1,37 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+import { GPTStream } from "@/lib/chatbot/methods";
+import { chatbotPrompt } from "@/lib/chatbot/prompt";
+import {
+  GPTMessage,
+  GPTMessageArraySchema,
+  GPTPayload
+} from "@/lib/chatbot/validators";
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  res.status(200).json({ message: "Hello" });
+  if (req.method === "POST") {
+    const body = await req.body;
+    const messages = GPTMessageArraySchema.safeParse(body);
+
+    if (!messages.success) {
+      return res.status(400).json({ message: "Bad Request" });
+    }
+
+    const outgoingMessages: GPTMessage[] = messages.data;
+    outgoingMessages.unshift(chatbotPrompt);
+
+    const payload: GPTPayload = {
+      model: "gpt-3.5-turbo",
+      messages: outgoingMessages,
+      stream: true
+    };
+
+    const incomingMessage = await GPTStream(payload);
+
+    return res.status(200).json({ message: incomingMessage });
+  }
+  return res.status(405).json({ message: `${req.method} Not Allowed` });
 }
