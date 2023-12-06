41921fc87743e4f209d43043f08bf5e7e02c6d3a
Amy Neve <ladyames20203@gmail.com>
2023-12-06 17:20:47 +0800

    attempt to separate out header

--- src/components/inpagenav/index.tsx
+++ src/components/inpagenav/index.tsx
@@ -4,22 +4,33 @@ import Link from "next/link";
 const inter = Inter({ subsets: ["latin"] });
 
 // todo: collapsable
+// focus: (been clicked on)
 // todo: styling
+
+// stuck trying to pass both header and list
 
 type listDetails = {
   href: string;
   name: string;
 };
 
-export default function InPageNav({ list }: { list: listDetails[] }) {
+export default function InPageNav(
+  header: string,
+  { list }: { list: listDetails[] }
+) {
   const inPageNavList = list.map((details) => {
     return (
-      <div
-        key={details.name}
-        className="rounded-md border-x-2 border-y border-light-green p-2 text-gray-600 first:border-t-2 last:border-b-2"
-      >
-        <Link href={details.href}>{details.name}</Link>
-      </div>
+      <>
+        <div className="rounded-md border-x-2 border-y border-light-green p-2 text-gray-600 first:border-t-2 first:font-semibold last:border-b-2">
+          {header}
+        </div>
+        <div
+          key={details.name}
+          className="rounded-md border-x-2 border-y border-light-green p-2 text-gray-600 first:border-t-2 first:font-semibold last:border-b-2"
+        >
+          <Link href={details.href}>{details.name}</Link>
+        </div>
+      </>
     );
   });
   return (
