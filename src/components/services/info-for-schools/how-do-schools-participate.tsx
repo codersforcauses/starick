4219b103b4ba424e0d5e4e79ed6776d0a8d9c4be
unknown <davema0522@163.com>
2023-12-09 13:56:01 +0800

    how do school participate section

---
+++ src/components/services/info-for-schools/how-do-schools-participate.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import ServicesSection from "../services-section"
+import SubSection from "../subsection"
+
+export default function HowDoSchoolsParticipate() {
+    const subbody = (<>
+        <p>Starick works with the Department of Education to identify schools that want to participate in the program. Participation in the program is voluntary and each school provides an Expression of Interest to show their interest. Schools that participate in the program are supported by Starick to develop an approach to Respectful Relationships Education (RRE) that suits their school&apos;s needs and environment. This includes support to develop plans to embed a whole-of-school approach that is relevant and sustainable.</p> 
+        <br />
+        <p>Since 2019, Starick has worked with 47 public schools grouped into five cohorts.</p>
+    </>)
+    const body = <SubSection iconPath="/icons/did-you-know.svg" sectionBody={subbody}/>
+    return <ServicesSection titleTextColour="[--starick-white]" titleBackgroundColour="[--starick-green]" sectionBody={body} title="How do schools participate in the program?" />
+}
--- src/pages/respectful-relationships/info-for-schools.tsx
+++ src/pages/respectful-relationships/info-for-schools.tsx
@@ -1,9 +1,12 @@
+import HowDoSchoolsParticipate from '@/components/services/info-for-schools/how-do-schools-participate';
 import Banner from '../../components/services/banner';
 import ProgramComponents from '../../components/services/info-for-schools/program-components';
 
 export default function InfoForSchools() {
     return <>
             <Banner titleText="Respectful relationships" subtitleText="Info for schools" />
+            <HowDoSchoolsParticipate />
             <ProgramComponents />
+           
         </>
 }
