312006d5fb5018903195efa4e27daa2aa9334740
HuxleyBerry <huxley.berry@gmail.com>
2023-12-09 12:47:44 +0800

    program components

---
+++ src/pages/respectful-relationships/info-for-schools.tsx
@@ -0,0 +1,9 @@
+import Banner from '../../components/services/banner';
+import ProgramComponents from '../../components/services/info-for-schools/program-components';
+
+export default function InfoForSchools() {
+    return <>
+            <Banner titleText="Respectful relationships" subtitleText="Info for schools" />
+            <ProgramComponents />
+        </>
+}
