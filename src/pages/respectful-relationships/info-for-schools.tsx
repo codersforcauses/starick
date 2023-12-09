42d9b2d6154e62e3b90f84eefa11cdc4749d63da
sheldonyxd <sheldon.yxdd@gmail.com>
2023-12-09 15:13:43 +0800

    Add what-our-school-achieved section.

--- src/pages/respectful-relationships/info-for-schools.tsx
+++ src/pages/respectful-relationships/info-for-schools.tsx
@@ -1,3 +1,4 @@
+import AchievedSection from '@/components/services/info-for-schools/what-we-achieved';
 import Banner from '../../components/services/banner';
 import ProgramComponents from '../../components/services/info-for-schools/program-components';
 
@@ -5,5 +6,6 @@ export default function InfoForSchools() {
     return <>
             <Banner titleText="Respectful relationships" subtitleText="Info for schools" />
             <ProgramComponents />
+            <AchievedSection />
         </>
 }
