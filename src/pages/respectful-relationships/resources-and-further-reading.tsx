ba68f5e2236336f33927f527f123059de34c52c6
HuxleyBerry <huxley.berry@gmail.com>
2023-12-09 14:04:32 +0800

    start on resources and further reading page

---
+++ src/pages/respectful-relationships/resources-and-further-reading.tsx
@@ -0,0 +1,9 @@
+import Banner from '../../components/services/banner';
+import EvidenceBase from '../../components/services/resources-and-further-learning/evidence-base';
+
+export default function InfoForSchools() {
+    return <>
+            <Banner titleText="Respectful relationships" subtitleText="Resources and Further Reading" />
+            <EvidenceBase />
+        </>
+}
