9081cc07bc519994a58bfc040fc1e2945e6ae6e3
John Smith <makon1234@users.noreply.github.com>
2023-12-09 14:43:58 +0800

    fixed typescript errors

--- src/components/messengerembed/messengerembed.tsx
+++ src/components/messengerembed/messengerembed.tsx
@@ -5,7 +5,7 @@ declare let window: FBWindow;
 
 export default function MessengerChat() {
   useEffect(() => {
-    var chatbox = document.getElementById("fb-customer-chat");
+    const chatbox = document.getElementById("fb-customer-chat");
     if (chatbox) {
       chatbox.setAttribute("page_id", "174020045800640");
       chatbox.setAttribute("attribution", "biz_inbox");
@@ -19,14 +19,13 @@ export default function MessengerChat() {
     };
 
     (function (d, s, id) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0];
+      const fts = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) return;
-      js = d.createElement(s) as HTMLScriptElement;
-      js.id = id;
-      js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-      if (fjs.parentNode) {
-        fjs.parentNode.insertBefore(js, fjs);
+      const ts = d.createElement(s) as HTMLScriptElement;
+      ts.id = id;
+      ts.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+      if (fts.parentNode) {
+        fts.parentNode.insertBefore(ts, fts);
       }
     })(document, "script", "facebook-jssdk");
   }, []);
