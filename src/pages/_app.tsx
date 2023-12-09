53f481ca61170d5ffb8cb36e0bfe49c042225bd1
dimmerz92 <andrew.weymes@outlook.com>
2023-12-09 20:55:08 +0800

    change main to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,9 +6,15 @@ import NavBar from "@/components/NavBar/NavBar";
 import PanicButton from "@/components/panicbutton/panicbutton";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (<><header>
-    <NavBar />
-    <PanicButton />
-  </header>
-  <Component {...pageProps} /></>);
+  return (
+    <>
+      <header>
+        <NavBar />
+        <PanicButton />
+      </header>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
