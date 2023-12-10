3abd889e2b8f6c20a606444d1dac68451ef3f0bb
moebuta <1059231176@qq.com>
2023-12-10 16:30:30 +0800

    optimise the font size in this component

--- src/components/donations/donationTitle.tsx
+++ src/components/donations/donationTitle.tsx
@@ -6,7 +6,7 @@ const DonationTitle: React.FC<{text:string}> = ({text}) => {
     return (
         <div
             className="flex bg-[--starick-green]  w-full h-[150px] p-4 items-center justify-center">
-            <p className="flex justify-center items-center  w-full h-full text-3xl md:text-4xl text-[--starick-white] font-semibold border-4 md:border-8 border-[--starick-white]">
+            <p className="flex justify-center items-center  w-full h-full text-2xl md:text-4xl text-[--starick-white] font-semibold border-4 md:border-8 border-[--starick-white]">
                 {text}
             </p>
         </div>
