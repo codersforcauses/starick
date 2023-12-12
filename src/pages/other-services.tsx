26d4602583bbe913817d41141b406c6f4df96654
K-Straiton <straiton98@gmail.com>
2023-12-12 20:19:49 +0800

    Create other-services.tsx page

---
+++ src/pages/other-services.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+import InPageNav from "@/components/InPageNav";
+
+
+export default function OtherServices() {
+    return (
+            <div>Other Services</div>
+    );
+}
