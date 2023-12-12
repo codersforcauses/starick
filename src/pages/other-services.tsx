32caf4857272633a291cb623a726347d9382fe58
K-Straiton <straiton98@gmail.com>
2023-12-12 20:54:55 +0800

    Added in page navigation

--- src/pages/other-services.tsx
+++ src/pages/other-services.tsx
@@ -4,7 +4,39 @@ import InPageNav from "@/components/InPageNav";
 
 
 export default function OtherServices() {
+    const navLinkDetails = [
+        {
+          href: "#WA Respectful Relationships Teaching Support Program",
+          name: "WA Respectful Relationships Teaching Support Program"
+        },
+        {
+          href: "#Our Work with Children and Young People",
+          name: "Our Work with Children and Young People"
+        },
+        {
+          href: "#Workplace Awareness",
+          name: "Workplace Awareness"
+        },
+        {
+          href: "#Health and Wellbeing",
+          name: "Health and Wellbeing"
+        },
+        {
+          href: "#Thriving Through Connection",
+          name: "Thriving Through Connection"
+        }
+      ];
     return (
-            <div>Other Services</div>
+        <>
+            <div>
+                <img src="images/starick-image1.jpg" className="w-full" alt=""></img>
+                <div className="relative -mt-20 mb-6 w-fit rounded-r-full bg-white px-14">
+                    <p className="p-2 text-4xl font-bold text-[--starick-brown]">
+                    Other Services
+                    </p>
+                </div>
+            </div>
+            <InPageNav list={navLinkDetails}></InPageNav>
+        </>
     );
 }
