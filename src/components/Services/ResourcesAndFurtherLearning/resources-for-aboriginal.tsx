d41f77b25b2c71e6d61f9a27ae2d4c4e84431c0d
HuxleyBerry <huxley.berry@gmail.com>
2023-12-13 19:03:35 +0800

    prettier

--- src/components/Services/ResourcesAndFurtherLearning/resources-for-aboriginal.tsx
+++ src/components/Services/ResourcesAndFurtherLearning/resources-for-aboriginal.tsx
@@ -8,14 +8,17 @@ export default function ResourcesForAboriginal() {
     <>
       <ul>
         <li className="list-inside list-decimal">
-          <Link href="https://www.respect.gov.au/wp-content/uploads/2021/03/aboriginal-and-torres-strait-islander-speak-booklet_0.pdf" className="my-link">
-          Speak up booklet
+          <Link
+            href="https://www.respect.gov.au/wp-content/uploads/2021/03/aboriginal-and-torres-strait-islander-speak-booklet_0.pdf"
+            className="my-link"
+          >
+            Speak up booklet
           </Link>{" "}
-          – a resource to help encourage and support people to speak up about 
-          disrespectful attitudes by having conversations about respectful 
+          – a resource to help encourage and support people to speak up about
+          disrespectful attitudes by having conversations about respectful
           relationships with the young people in their community.
         </li>
-        
+
         <li className="list-inside list-decimal">
           <Link
             href="https://www.respect.gov.au/wp-content/uploads/2016/11/atsi_storybook.pdf"
@@ -23,10 +26,10 @@ export default function ResourcesForAboriginal() {
           >
             Respect starts with us
           </Link>{" "}
-          – a storybook to help model how to address disrespectful behaviour and 
+          – a storybook to help model how to address disrespectful behaviour and
           start the conversation about showing respect to women and girls.{" "}
         </li>
-        
+
         <li className="list-inside list-decimal">
           <Link
             href="https://www.respect.gov.au/wp-content/uploads/2016/06/atsi_conversation_guide.pdf"
@@ -34,39 +37,42 @@ export default function ResourcesForAboriginal() {
           >
             Respecting women and girls:
           </Link>{" "}
-          Talking with our young people – a conversation guide to talk with young people about respect. 
-          Short guides include: {" "}
+          Talking with our young people – a conversation guide to talk with
+          young people about respect. Short guides include:{" "}
           <Link
             href="https://www.respect.gov.au/wp-content/uploads/2018/09/indigenous-short-guide-one-be-ready-talk.pdf"
             className="my-link"
           >
             Be ready to talk
-          </Link>, {" "}
+          </Link>
+          ,{" "}
           <Link
             href="https://www.respect.gov.au/wp-content/uploads/2018/10/starting-talk.pdf"
             className="my-link"
           >
             Starting the talk
-          </Link>, and {" "}
+          </Link>
+          , and{" "}
           <Link
             href="https://www.respect.gov.au/wp-content/uploads/2018/10/keep-yarning.pdf"
             className="my-link"
           >
             Keep yarning
-          </Link>.
+          </Link>
+          .
         </li>
-        
+
         <li className="list-inside list-decimal">
           <Link
             href="https://www.respect.gov.au/wp-content/uploads/2018/07/atsi_excuse_interpreter.pdf"
             className="my-link"
           >
-            Understanding our excuses 
+            Understanding our excuses
           </Link>{" "}
-          – a guide to learn about ways we overlook disrespectful behaviour towards 
-          women and girls without knowing it.
+          – a guide to learn about ways we overlook disrespectful behaviour
+          towards women and girls without knowing it.
         </li>
-        
+
         <li className="list-inside list-decimal">
           <Link
             href="https://www.respect.gov.au/wp-content/uploads/2016/06/atsi_the_respect_checklist.pdf"
@@ -74,7 +80,7 @@ export default function ResourcesForAboriginal() {
           >
             The respect checklist
           </Link>{" "}
-          – a practical checklist to help parents and family members identify 
+          – a practical checklist to help parents and family members identify
           some aspects of respect to talk about with children.
         </li>
       </ul>
