65944dac150da89a45489861bb1d4807b6fe146b
John Smith <makon1234@users.noreply.github.com>
2023-12-13 14:23:37 +0800

    cleaned code

--- src/components/messengerembed/types.ts
+++ src/components/messengerembed/types.ts
@@ -5,17 +5,3 @@ export interface FBWindow extends Window {
     init: (options: { xfbml: boolean; version: string }) => void;
   };
 }
-
-// import { FacebookStatic } from 'facebook';
-// export type FBWindow = {
-//   fbAsyncInit: () => void;
-//   FB: FacebookStatic;
-// };
-
-// import fb = facebook;
-// declare var FB: fb.FacebookStatic;
-
-// export type FB = {
-//   fbAsyncInit: any;
-//   FB: any;
-// };
