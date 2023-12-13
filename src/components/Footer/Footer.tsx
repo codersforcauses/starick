3d38d2143a7400132fd6ac56efcfbe36caab8af4
yunho <gurumindc123@gmail.com>
2023-12-14 02:18:36 +0800

    fix some grid layout problem

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <>
-      <footer className="grid bg-[#C3C9A0] p-4 xl:grid-cols-[auto_0.8fr_1fr]">
-        <div className=" xl:ml-6 text-center ">
+      <footer className="grid bg-[#C3C9A0] p-4 lg:grid-cols-[2fr_1.2fr_1fr] ">
+        <div className=" text-center md:ml-6 ">
           <div className=" flex flex-wrap items-center justify-center p-5 ">
             <div className=" w-auto overflow-hidden  ">
               <img
@@ -27,7 +27,7 @@ const Footer = () => {
             </p>
           </div>
 
-          <p className="  sm:ml-[-4rem] font-normal text-black   ">
+          <p className="  font-normal text-black   ">
             © 2023 Starick. All Rights Reserved. |{" "}
             <a href="#" className="hover:underline">
               Privacy & Disclaimer
@@ -35,21 +35,34 @@ const Footer = () => {
           </p>
         </div>
         <div className="m-3 text-center ">
-          <p className="min-w-min hover:underline  "><a href="#">Site map / links to pages here</a></p>
-          <p className="min-w-min hover:underline  "><a href="#">Site map / links to pages here</a></p>{" "}
-          <p className="min-w-min hover:underline  "><a href="#">Site map / links to pages here</a></p>{" "}
-          <p className="min-w-min hover:underline  "><a href="#">Site map / links to pages here</a></p>
+          <p className="min-w-min hover:underline  ">
+            <a href="#">Site map / links to pages here</a>
+          </p>
+          <p className="min-w-min hover:underline  ">
+            <a href="#">Site map / links to pages here</a>
+          </p>{" "}
+          <p className="min-w-min hover:underline  ">
+            <a href="#">Site map / links to pages here</a>
+          </p>{" "}
+          <p className="min-w-min hover:underline  ">
+            <a href="#">Site map / links to pages here</a>
+          </p>
         </div>
-        <div className=" mx-auto text-center xl:ml-6 xl:text-left my-5 mb-9 ">
-          <p className="text-base font-normal text-black mb-4">
+        <div className=" mx-auto my-5 mb-9  lg:ml-6 lg:text-left ">
+          <p className="mb-4 text-base font-normal text-black text-left">
             Subscribe to our newsletter
           </p>
-          <input
-            className="  border-0 py-1.5 pl-4 pr-10 text-gray-900  placeholder:text-gray-400   sm:text-sm sm:leading-6"
-            placeholder="youremail@gmail.com"
-            type="text"
-          />
-          <button className="px-5 py-2 font-semibold text-sm bg-[#738C4B] text-white rounded-none shadow-sm ml-0">Submit</button>
+          <section className=" overflow-hidden min-w-[100px] flex ">
+            <input
+              type="email"
+              name="email"
+              className=" px-5 py-2 placeholder-slate-400 shadow-sm  focus:outline-none focus:ring-1 sm:text-sm"
+              placeholder="you@example.com"
+            />
+            <button className="ml-0 rounded-none bg-[#738C4B] px-5 py-2 text-sm font-semibold text-white shadow-sm">
+              Submit
+            </button>
+          </section>
         </div>
       </footer>
     </>
