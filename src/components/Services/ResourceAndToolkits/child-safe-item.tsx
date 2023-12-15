6f5efb3c4c2d25ca00ed1c0505849efcf566375b
moebuta <1059231176@qq.com>
2023-12-15 19:16:17 +0800

    fixing format issues

--- src/components/Services/ResourceAndToolkits/child-safe-item.tsx
+++ src/components/Services/ResourceAndToolkits/child-safe-item.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const ChildSafeItem: React.FC<{ background: string, text: string }> = ({background, text}) => {
+const ChildSafeItem: React.FC<{ backgroundColor: string, text: string }> = ({background, text}) => {
     return (
-        <p className={`flex h-full w-full items-center px-10 py-2 bg-[--${background}]`} dangerouslySetInnerHTML={{ __html: text }}/>
+        <p className={`flex h-full w-full items-center px-10 py-2 bg-[--starick-${background}]`} dangerouslySetInnerHTML={{ __html: text }}/>
 
     );
 };
--- src/pages/other-services/resources-and-toolkits/child-safe-resources.tsx
+++ src/pages/other-services/resources-and-toolkits/child-safe-resources.tsx
@@ -68,69 +68,69 @@ export default function ChildSafeResources() {
             </div>
             <div className="px-10 md:px-20 lg:px-32">
                 <SectionTitle text="Child Safe Template"
-                              backgroundColour={"[--starick-green]"} textColour={"[--starick-white]"}
-                              iconPath={"/images/ResourcesToolkits/child-safe-templates-icon.png"}/>
+                              backgroundColour="[--starick-green]" textColour="[--starick-white]"
+                              iconPath="/images/ResourcesToolkits/child-safe-templates-icon.png"/>
             </div>
 
             <div className="px-20 py-10 md:px-40 md:py-20 lg:px-60">
-                <ChildSafeItem background={"starick-olive"}
+                <ChildSafeItem backgroundColor="olive"
                                text={"Child Safe Principle 2 - Self assessment&nbsp;<strong>FDV Board and management audit</strong>"}/>
-                <ChildSafeItem background={"starick-white"}
+                <ChildSafeItem backgroundColor="white"
                                text={"Child Safe Principle 2 - Self assessment&nbsp;<strong>FDV Service audit</strong>"}/>
-                <ChildSafeItem background={"starick-olive"}
+                <ChildSafeItem backgroundColor="olive"
                                text={"Child Safe&nbsp;<strong>Organisation Procedure</strong>&nbsp;template"}/>
-                <ChildSafeItem background={"starick-white"}
+                <ChildSafeItem backgroundColor="white"
                                text={"Child Safe&nbsp;<strong>Organisation Policy</strong>&nbsp;template"}/>
-                <ChildSafeItem background={"starick-olive"}
+                <ChildSafeItem backgroundColor="olive"
                                text={"Child Safe&nbsp;<strong>Code of Conduct Policy</strong>&nbsp;template"}/>
-                <ChildSafeItem background={"starick-white"}
+                <ChildSafeItem backgroundColor="white"
                                text={"Sample WHFS&nbsp;<strong>Policy for Child Safety</strong>"}/>
-                <ChildSafeItem background={"starick-olive"}
+                <ChildSafeItem backgroundColor="olive"
                                text={"Sample WHFS&nbsp;<strong>Code of Conduct Policy</strong>"}/>
-                <ChildSafeItem background={"starick-white"}
+                <ChildSafeItem backgroundColor="white"
                                text={"Sample WHFS&nbsp;<strong>Incident Reporting Policy</strong>"}/>
-                <ChildSafeItem background={"starick-olive"}
+                <ChildSafeItem backgroundColor="olive"
                                text="<strong>Empowerment and Participation of Children and Young People Policy</strong>"/>
-                <ChildSafeItem background={"starick-white"}
+                <ChildSafeItem backgroundColor="white"
                                text={"Children and Young People&nbsp;<strong>Safety and Wellbeing Policy</strong>"}/>
 
                 <p className="my-10 text-2xl text-center font-semibold">Practice Tools</p>
 
-                <ChildSafeItem background={"starick-olive"}
+                <ChildSafeItem backgroundColor="olive"
                                text={"<strong>Practice Tool 1</strong>&nbsp;- Building Relationships"}/>
-                <ChildSafeItem background={"starick-white"}
+                <ChildSafeItem backgroundColor="white"
                                text={"<strong>Practice Tool 2</strong>&nbsp;- Empowerment & Participation"}/>
-                <ChildSafeItem background={"starick-olive"}
+                <ChildSafeItem backgroundColor="olive"
                                text={"<strong>Practice Tool 3</strong>&nbsp;- Informed About Their Rights"}/>
 
             </div>
 
 
             <div className="px-10 md:px-20 lg:px-32">
-                <SectionTitle text="Posters" backgroundColour={"[--starick-brown]"}
-                              textColour={"[--starick-white]"}
-                              iconPath={"/images/ResourcesToolkits/child-safe-templates-icon.png"}/>
+                <SectionTitle text="Posters" backgroundColour="[--starick-brown]"
+                              textColour="[--starick-white]"
+                              iconPath="/images/ResourcesToolkits/child-safe-templates-icon.png"/>
             </div>
 
 
             <div className="px-20 py-10 md:px-40 md:py-20 lg:px-60">
-                <ChildSafeItem background={"starick-olive"} text="UN Convention on the Rights of the Child"/>
+                <ChildSafeItem backgroundColor="olive" text="UN Convention on the Rights of the Child"/>
 
             </div>
 
             <div className="px-10 md:px-20 lg:px-32">
                 <SectionTitle text="External Resources"
-                              backgroundColour={"[--starick-olive]"} textColour={"black"}
-                              iconPath={"/images/ResourcesToolkits/child-safe-templates-icon.png"}/>
+                              backgroundColour="[--starick-olive]" textColour="black"
+                              iconPath="/images/ResourcesToolkits/child-safe-templates-icon.png"/>
             </div>
 
             <div className="px-20 py-10 md:px-40 md:py-20 lg:px-60">
-                <ChildSafeItem background={"starick-olive"}
+                <ChildSafeItem backgroundColor="olive"
                                text="Child friendly version of the Children's Safety and Wellbeing report"/>
-                <ChildSafeItem background={"starick-white"} text="Commonwealth Child Safe Framework"/>
-                <ChildSafeItem background={"starick-olive"}
+                <ChildSafeItem backgroundColor="white" text="Commonwealth Child Safe Framework"/>
+                <ChildSafeItem backgroundColor="olive"
                                text="Learning from lived experience - a guide for professionals supporting children and young people experiencing family violence"/>
-                <ChildSafeItem background={"starick-white"} text="Commonwealth Child safe framework 2nd edition"/>
+                <ChildSafeItem backgroundColor="white" text="Commonwealth Child safe framework 2nd edition"/>
 
             </div>
 
