a835d4a541568477c77e2ff3a3b0804513e12b29
dimmerz92 <andrew.weymes@outlook.com>
2023-12-15 12:35:38 +0800

    add rate limiter to prevent enter spam

--- src/components/ChatBot/assistant.tsx
+++ src/components/ChatBot/assistant.tsx
@@ -51,6 +51,8 @@ function Assistant() {
   const handleEnterKey = async (event: React.KeyboardEvent) => {
     if (event.key == "Enter" && !event.shiftKey) {
       event.preventDefault();
+      const lastMessage = messages.slice(-1)[0].role;
+      if (lastMessage !== "assistant" || userText.length < 3) return; // prevent spamming of enter button
       const userMessage: GPTMessage = { role: "user", content: userText };
       setUserText("");
       setMessages((prev) => [...prev, userMessage]);
