66d6fe3130811209d66746d990de98b0da026bf1
moebuta <1059231176@qq.com>
2023-12-15 19:28:19 +0800

    fixing argument issue and formatting code

--- src/components/Services/ResourceAndToolkits/child-safe-item.tsx
+++ src/components/Services/ResourceAndToolkits/child-safe-item.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 
-export default function ChildSafeItem({backgroundColor, text}) {
-    return (
-        <p className={`flex h-full w-full items-center px-10 py-2 bg-[--starick-${backgroundColor}]`} dangerouslySetInnerHTML={{ __html: text }}/>
-    );
+interface ChildSafeItemProps {
+  backgroundColor: string;
+  text: string;
 }
 
+export default function ChildSafeItem({
+  backgroundColor,
+  text
+}: ChildSafeItemProps) {
+  return (
+    <p
+      className={`flex h-full w-full items-center px-10 py-2 bg-[--starick-${backgroundColor}]`}
+      dangerouslySetInnerHTML={{ __html: text }}
+    />
+  );
+}
