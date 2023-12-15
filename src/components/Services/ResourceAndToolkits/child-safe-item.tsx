619ee2b917e599f4fad51c50f5110c0d0f35c79c
moebuta <1059231176@qq.com>
2023-12-15 19:20:56 +0800

    fixing function format issue

--- src/components/Services/ResourceAndToolkits/child-safe-item.tsx
+++ src/components/Services/ResourceAndToolkits/child-safe-item.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 
-const ChildSafeItem: React.FC<{ backgroundColor: string, text: string }> = ({backgroundColor, text}) => {
+export default function ChildSafeItem({backgroundColor, text}) {
     return (
         <p className={`flex h-full w-full items-center px-10 py-2 bg-[--starick-${backgroundColor}]`} dangerouslySetInnerHTML={{ __html: text }}/>
-
     );
-};
-export default ChildSafeItem;
+}
+
