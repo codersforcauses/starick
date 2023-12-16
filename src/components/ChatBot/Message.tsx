d6f25e58c646dee0625d081e221c0e48b6ae3d42
dimmerz92 <andrew.weymes@outlook.com>
2023-12-16 14:15:56 +0800

    update message bubble styles

--- src/components/ChatBot/Message.tsx
+++ src/components/ChatBot/Message.tsx
@@ -3,14 +3,14 @@ import React from "react";
 import { GPTMessage } from "@/lib/chatbot/validators";
 
 const styles = {
-  user: "bg-lime-700 self-end",
+  user: "bg-[--starick-orange] text-white self-end",
   system: "bg-rose-400 self-center",
-  assistant: "bg-cyan-600"
+  assistant: "bg-[--messenger-grey] text-[--starick-black]"
 };
 
 function Message({ role, content }: GPTMessage) {
   return (
-    <div className={`${styles[role]} max-w-[80%] rounded-md p-1 text-white`}>
+    <div className={`${styles[role]} max-w-[80%] rounded-md p-1`}>
       {content}
     </div>
   );
