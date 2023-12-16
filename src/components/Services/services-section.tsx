dfcdf79d5610461466474e4fc76a34e453cf34ba
Amy Neve <ladyames20203@gmail.com>
2023-12-16 12:40:28 +0800

    fixed id line break

--- src/components/Services/services-section.tsx
+++ src/components/Services/services-section.tsx
@@ -18,7 +18,10 @@ export default function Section({
   iconPath
 }: SectionProps) {
   return (
-    <div className="bg-[--starick-white] px-6 py-2 md:px-32" id={title}>
+    <div
+      className="bg-[--starick-white] px-6 py-2 md:px-32"
+      id={title.replace("\n", "")}
+    >
       <SectionTitle
         backgroundColour={titleBackgroundColour}
         text={title}
