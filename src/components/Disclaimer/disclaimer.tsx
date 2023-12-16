922e568ab8b97b6d76d85e29ea710351b0559d97
sersang <sersang01@gmail.com>
2023-12-16 14:24:12 +0800

    Adding the disclaimer component

---
+++ src/components/Disclaimer/disclaimer.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Disclaimer = () => {
+    return (
+        <>
+        <div className="font-bold bg-[--starick-brown] text-white text-center">
+            <p>Disclaimer</p>
+        </div>
+      <div className="bg-[--starick-brown] text-white text-center">
+        <p>This content is for educational and informational purposes it is not intended as a substitute for professional advice.</p>
+        <br></br>
+        <p> While the information has been verified to the best of our abilities, we cannot guarantee there are no mistakes or errors.</p>
+        <br></br>
+        <p>We recommend that users exercise their own judgment and use their skills, knowledge and experience to evaluate the accuracy, currency, completeness, relevance and suitability of the information for their needs.</p>
+        <br></br>
+        <p>We reserve the right to change this information at any time.</p>
+      </div>
+      </>
+    );
+  };
+  
+  export default Disclaimer;
