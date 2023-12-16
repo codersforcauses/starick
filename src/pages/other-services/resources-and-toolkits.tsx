bd6b93a9c104d0efd083757b0c4d238fe3265d44
sersang <sersang01@gmail.com>
2023-12-16 13:16:30 +0800

    Added photos for the toolkits page and some formatting

--- src/pages/other-services/resources-and-toolkits.tsx
+++ src/pages/other-services/resources-and-toolkits.tsx
@@ -4,6 +4,7 @@ export default function ResourcesAndToolkits() {
   return (
     <>
       <div>
+          {/* Header */}
         <img src="/images/starick-image1.jpg" className="w-full" alt=""></img>
         <div className="relative -mt-36 mb-6 w-fit rounded-r-full bg-white px-10">
           <p className="p-2 text-4xl font-bold text-[--starick-brown]">
@@ -14,43 +15,51 @@ export default function ResourcesAndToolkits() {
           </p>
         </div>
       </div>
+
+      {/* Images */}
       {/* Child Safe Organisation Resources */}
+      <br></br>
+      <br></br>
       <div className="-mx-2 flex flex-wrap justify-center bg-[--starick-white]">
-        <div className="w-1/2 px-8 md:w-1/3 lg:w-1/6">
+        <div className="w-1/2 px-4 md:w-1/3 lg:w-1/6">
           <a href="https://www.starick.org.au/wp-content/uploads/STARICK-Covid-Womens-Resource11.7.22.pdf">
             <img
               className="block h-auto w-full"
-              src="/images/covid-19-pdf-English.png"
+              src="/images/child-safe-organisation-resources.png"
               alt=""
             ></img>
           </a>
         </div>
         {/* Children's Participation and Engagement */}
-        <div className="w-1/2 px-8 md:w-1/3 lg:w-1/6">
+        <div className="w-1/2 px-4 md:w-1/3 lg:w-1/6">
           <a href="https://www.starick.org.au/wp-content/uploads/COVID-womens-resource-Chinese-Simplified.pdf">
             <img
               className="block h-auto w-full"
-              src="/images/covid-19-pdf-Chinese.png"
+              src="/images/children's-participation-and-engagement.png"
               alt=""
             ></img>
           </a>
         </div>
+      </div>
+
+      <br></br>
+      <div className="-mx-2 flex flex-wrap justify-center bg-[--starick-white]">
         {/* Covid-19 Kit for Children & Young People        - pdf*/}
-        <div className="w-1/2 px-8 md:w-1/3 lg:w-1/6">
+        <div className="w-1/2 px-4 md:w-1/3 lg:w-1/6">
           <a href="https://www.starick.org.au/wp-content/uploads/STARICK-Covid-Womens-Resource-ARABIC.pdf">
             <img
               className="block h-auto w-full"
-              src="/images/covid-19-pdf-Arabic.png"
+              src="/images/covid-19-kit-for-children-&-young-people.png"
               alt=""
             ></img>
           </a>
         </div>
         {/* Covid-19 Kit for Parents */}
-        <div className="w-1/2 px-8 md:w-1/3 lg:w-1/6">
+        <div className="w-1/2 px-4 md:w-1/3 lg:w-1/6">
           <a href="https://www.starick.org.au/wp-content/uploads/STARICK-Covid-Womens-booklet-FARSI.pdf">
             <img
               className="block h-auto w-full"
-              src="/images/covid-19-pdf-Farsi.png"
+              src="/images/covid-19-kit-for-parents.png"
               alt=""
             ></img>
           </a>
