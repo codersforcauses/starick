23a1336887332a55d06af2283b3d75126d6f00bd
John Smith <makon1234@users.noreply.github.com>
2023-12-16 12:36:16 +0800

    uncomment code from main

--- src/lib/chatbot/methods.ts
+++ src/lib/chatbot/methods.ts
@@ -1,21 +1,21 @@
-// import { GPTPayload, GPTResponseSchema } from "./validators";
+import { GPTPayload, GPTResponseSchema } from "./validators";
 
-// export async function fetchGPTResponse(payload: GPTPayload) {
-//   const res = await fetch("https://api.openai.com/v1/chat/completions", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//       Authorization: `Bearer ${process.env.OPENAI_API_KEY}`
-//     },
-//     body: JSON.stringify(payload)
-//   });
+export async function fetchGPTResponse(payload: GPTPayload) {
+  const res = await fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`
+    },
+    body: JSON.stringify(payload)
+  });
 
-//   if (!res.ok) return { success: false, message: "Unexpected Error" };
+  if (!res.ok) return { success: false, message: "Unexpected Error" };
 
-//   const data = await res.json();
-//   const message = GPTResponseSchema.safeParse(data);
+  const data = await res.json();
+  const message = GPTResponseSchema.safeParse(data);
 
-//   if (!message.success) return { success: false, message: "Unexpected Error" };
+  if (!message.success) return { success: false, message: "Unexpected Error" };
 
-//   return { success: true, message: message.data.choices[0].message };
-// }
+  return { success: true, message: message.data.choices[0].message };
+}
--- src/pages/api/paypal/client.ts
+++ src/pages/api/paypal/client.ts
@@ -1,15 +1,15 @@
-// import checkoutNodeJssdk from "@paypal/checkout-server-sdk";
+import checkoutNodeJssdk from "@paypal/checkout-server-sdk";
 
-// const configureEnvironment = () => {
-//     const clientId = process.env.PAYPAL_CLIENT_ID as string;
-//     const clientSecret = process.env.PAYPAL_CLIENT_SECRET as string;
+const configureEnvironment = () => {
+    const clientId = process.env.PAYPAL_CLIENT_ID as string;
+    const clientSecret = process.env.PAYPAL_CLIENT_SECRET as string;
 
-//     return process.env.NODE_ENV === "production"
-//         ? new checkoutNodeJssdk.core.LiveEnvironment(clientId, clientSecret)
-//         : new checkoutNodeJssdk.core.SandboxEnvironment(clientId, clientSecret);
-// };
+    return process.env.NODE_ENV === "production"
+        ? new checkoutNodeJssdk.core.LiveEnvironment(clientId, clientSecret)
+        : new checkoutNodeJssdk.core.SandboxEnvironment(clientId, clientSecret);
+};
 
-// const client = () =>
-//     new checkoutNodeJssdk.core.PayPalHttpClient(configureEnvironment());
+const client = () =>
+    new checkoutNodeJssdk.core.PayPalHttpClient(configureEnvironment());
 
-// export default client;
+export default client;
