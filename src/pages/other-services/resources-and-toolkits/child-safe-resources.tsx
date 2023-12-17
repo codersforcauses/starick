27ddfabaac8903e19e5b2fc625151c523f8e5c2d
moebuta <1059231176@qq.com>
2023-12-17 11:29:58 +0800

    extract disclaimer component

--- src/pages/other-services/resources-and-toolkits/child-safe-resources.tsx
+++ src/pages/other-services/resources-and-toolkits/child-safe-resources.tsx
@@ -8,6 +8,7 @@ import ChildSafeList from "@/components/Services/ResourceAndToolkits/child-safe-
 
 import ChildSafeResourcesTitle from "~/images/ResourcesToolkits/child-safe-resources-title.png";
 import ChildSafeItem from "@/components/Services/ResourceAndToolkits/child-safe-item";
+import Disclaimer from "@/components/Services/ResourceAndToolkits/disclaimer";
 
 export default function ChildSafeResources() {
   const emailAddress = "info@starick.org.au";
@@ -128,30 +129,7 @@ export default function ChildSafeResources() {
         <ChildSafeList itemList={externalResources} />
       </div>
 
-      <div className="h-auto w-full bg-[--starick-brown] px-20 py-10 text-[--starick-white] md:px-40 lg:px-60">
-        <p className="text-center text-xl font-semibold">Disclaimer</p>
-
-        <p className="mb-4 mt-6">
-          This content is for educational and informational purposes; it is not
-          intended as a substitute for professional advice.
-        </p>
-
-        <p className="mb-4">
-          While the information has been verified to the best of our abilities,
-          we cannot guarantee there are no mistakes or errors.
-        </p>
-
-        <p className="mb-4">
-          We recommend that users exercise their own judgment and use their
-          skills, knowledge, and experience to evaluate the accuracy, currency,
-          completeness, relevance, and suitability of the information for their
-          needs.
-        </p>
-
-        <p className="mb-4">
-          We reserve the right to change this information at any time.
-        </p>
-      </div>
+      <Disclaimer />
     </>
   );
 }
