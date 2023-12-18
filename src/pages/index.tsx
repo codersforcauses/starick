a249db937d078f1cf85354c396d29467bb785bac
yunho <gurumindc123@gmail.com>
2023-12-18 22:17:15 +0800

    delete footer in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import { Inter } from "next/font/google";
 
-import Footer from "@/components/Footer/Footer";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -12,7 +10,6 @@ export default function Home() {
       >
         <h1 className="bg-red-500">Hello!</h1>
       </div>
-
     </>
   );
 }
