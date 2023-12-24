a97d133d0c25fd36dfcbd3e90617829ba91a072a
James Lee <jameslee6352@gmail.com>
2023-12-24 17:49:33 +0800

    Switching to Tailwind colour conventions

--- src/components/ChatBot/message.tsx
+++ src/components/ChatBot/message.tsx
@@ -3,9 +3,9 @@ import React from "react";
 import { GPTMessage } from "@/lib/chatbot/validators";
 
 const styles = {
-  user: "bg-[--starick-orange] text-white self-end",
+  user: "bg-starick-orange text-white self-end",
   system: "bg-rose-400 self-center",
-  assistant: "bg-[--messenger-grey] text-[--starick-black]"
+  assistant: "bg-messenger-grey text-starick-black"
 };
 
 function Message({ role, content }: GPTMessage) {
