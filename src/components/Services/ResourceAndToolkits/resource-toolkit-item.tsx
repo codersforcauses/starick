a5124e093a823dfc5e1260ef55317fa4a7057739
HuxleyBerry <huxley.berry@gmail.com>
2024-01-06 14:11:20 +0800

    fix text overlapping image on mobile and change from img to next/image

--- src/components/Services/ResourceAndToolkits/resource-toolkit-item.tsx
+++ src/components/Services/ResourceAndToolkits/resource-toolkit-item.tsx
@@ -1,10 +1,12 @@
+import Image from 'next/image'
+
 export default function ResourceToolkitItem({ text, backgroundColor, imageUrl, linkUrl }: { text: string; backgroundColor: string; imageUrl: string; linkUrl: string }) {
   return (
-    <div className={`flex flex-row bg-${backgroundColor} h-full items-center`}>
-      <a href={linkUrl} target="_blank" rel="noopener">
-        <img src={imageUrl} alt={text} className="my-2 ml-8 h-28 w-20 cursor-pointer" />
+    <div className={`flex flex-row bg-${backgroundColor} pl-8 pr-2 h-fit items-center h-200`}>
+      <a href={linkUrl} target="_blank" rel="noopener" className="shrink-0">
+        <Image src={imageUrl} alt={text} className="my-2 cursor-pointer" width={72} height={100}/>
       </a>
-      <p className="ml-4">{text}</p>
+      <p className="ml-4">  {text}</p>
     </div>
   );
 }
