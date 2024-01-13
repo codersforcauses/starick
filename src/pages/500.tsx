377ba805bb2e4fb9cf76628362e9c2f36723f031
James Lee <jameslee6352@gmail.com>
2024-01-13 13:42:13 +0800

    Switching to custom error pages

---
+++ src/pages/500.tsx
@@ -0,0 +1,8 @@
+export default function custom500() {
+  return (
+    <div className="flex h-full flex-row content-center justify-center gap-2 p-24 text-starick-brown">
+      <h1>Error 500:</h1>
+      <p>Internal server error</p>
+    </div>
+  );
+}
