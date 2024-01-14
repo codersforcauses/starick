e66b4be009df6239845864665e56ef73002f52ee
cjnbennett <conor.jn.bennett@gmail.com>
2024-01-14 17:17:44 +0800

    Added optimised frontend production Dockerfile

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  distDir: 'build',
+  output: 'standalone',
 };
 
 export default nextConfig;
