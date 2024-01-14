7fe8be9f9e29aa7c08d0ae048aa3e1ed8ce63df5
cjnbennett <conor.jn.bennett@gmail.com>
2024-01-14 17:19:32 +0800

    Formatting, excepted new build dir from formatter

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: 'build',
-  output: 'standalone',
+  distDir: "build",
+  output: "standalone"
 };
 
 export default nextConfig;
