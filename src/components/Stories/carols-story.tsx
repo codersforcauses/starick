36d28c9ba1f0c32bdff79a5622e00d350e0e3ed8
Huxley Berry <134905354+HuxleyBerry@users.noreply.github.com>
2024-01-14 08:45:43 +0800

    Issue 92 our stories page (#99)
    * set up stories page
    
    * annes story
    
    * carol and helena's stories
    
    * barbara's story and change spacing between lines
    
    * go back to using br tags for line breaks
    
    * run prettier
    
    * fix linting error
    
    * taranehs story
    
    ---------
    
    Co-authored-by: unknown <davema0522@163.com>

---
+++ src/components/Stories/carols-story.tsx
@@ -0,0 +1,50 @@
+import ServicesSection from "../Services/services-section";
+
+export default function CarolsStory() {
+  const body = (
+    <>
+      <p className="mx-10 my-6 text-center text-lg">
+        Carol was a resident at our Starick House Refuge. We have changed her
+        name to protect her identity.
+      </p>
+      <br />
+      <p>
+        Carol and her two children escaped her ex-husband, who was very violent
+        towards her and their children.
+      </p>
+      <br />
+      <p>
+        She arrived at our Starick House Refuge shy, distraught and upset, but
+        quickly came out of her shell, and grew in confidence as she was no
+        longer forced to live in the violent and abusive environment.
+      </p>
+      <br />
+      <p>
+        The children were courageous and strong, and showed resilience in
+        settling into their new environment.
+      </p>
+      <br />
+      <p>
+        Inevitably, they were also sleeping well and doing much better at school
+        after leaving the family home.
+      </p>
+      <br />
+      <p>
+        With support from Starick staff, Carol’s next step is to create a future
+        free from violence for herself and her children. She is keen to
+        rediscover the independence stolen from her, get a job, move into her
+        own home and, in her own words, “never look back!”
+      </p>
+      <br />
+      <p>Here’s what she said about her experience at Starick House:</p>
+    </>
+  );
+  return (
+    <ServicesSection
+      titleTextColour="starick-white"
+      titleBackgroundColour="starick-green"
+      sectionBody={body}
+      title="Carol's Story"
+    />
+  );
+}
