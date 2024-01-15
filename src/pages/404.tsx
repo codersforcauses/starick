baf9e895f47b46abe20b91838b906bf43d0fbd14
James Lee <jameslee6352@gmail.com>
2024-01-15 13:19:12 +0800

    Uncentred 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,8 @@
 export default function custom404() {
   return (
-    <div className="flex h-full flex-row content-center justify-center gap-2 p-24 text-starick-brown">
-      <h1>Error 404:</h1>
-      <p>Page could not be found</p>
+    <div className="static flex items-stretch h-full flex-row justify-center gap-2 text-starick-brown">
+      <h1 className="h-full">Error 404:</h1>
+      <p className="h-full">Page could not be found</p>
     </div>
   );
 }
