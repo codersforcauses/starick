c155086333f81b142ebbdb044a640d07d3e34f5f
Julie-Salazar <salazarfrancojuliana@gmail.com>
2024-01-18 13:28:39 +0800

    added module for svg

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,2 +1,11 @@
 declare module "*.jpg";
 declare module "*.png";
+declare module "*.svg" {
+    import * as React from "react";
+  
+    const ReactComponent: React.FunctionComponent<
+      React.SVGProps<SVGSVGElement> & { title?: string }
+    >;
+  
+    export default ReactComponent;
+  }
