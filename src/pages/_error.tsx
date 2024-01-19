1d8748b9ecc6d1178808c13baeeebb6fbb6957d5
James Lee <jameslee6352@gmail.com>
2024-01-20 03:14:56 +0800

    Formatting

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,22 +1,26 @@
-import { NextPageContext } from "next"
-import { ErrorProps } from "next/error"
+import { NextPageContext } from "next";
+import { ErrorProps } from "next/error";
 
 function Error({ statusCode }: ErrorProps) {
   return (
     <div className="flex flex-grow flex-row items-center justify-center gap-4 text-starick-brown">
-      <h1 className="text-3xl font-bold">Error{statusCode ? ` ${statusCode}` : ""}:</h1>
-      <p>{statusCode==404 ? 
-        "Page could not be found" 
-        : statusCode ? 
-        "Server encountered an error" 
-        : "Client encountered an error"}</p>
+      <h1 className="text-3xl font-bold">
+        Error{statusCode ? ` ${statusCode}` : ""}:
+      </h1>
+      <p>
+        {statusCode == 404
+          ? "Page could not be found"
+          : statusCode
+          ? "Server encountered an error"
+          : "Client encountered an error"}
+      </p>
     </div>
-  )
+  );
 }
 
 Error.getInitialProps = ({ res, err }: NextPageContext) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404
-  return { statusCode }
-}
- 
-export default Error
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
+};
+
+export default Error;
