908b7c3e1ec215c7bd30b929b5edec6d18036a80
unknown <davema0522@163.com>
2024-01-19 22:53:23 +0800

    stategic plans

--- src/components/OurPublications/strategic-plans.tsx
+++ src/components/OurPublications/strategic-plans.tsx
@@ -1,15 +1,9 @@
 import ServicesSection from "../Services/services-section";
 
-export default function StrategicPlans(){
-    const body = (
-        <>
-        <p>
-        Our domestic violence advocates are based at local police stations and
-        offer an advocacy service, safety planning and case management with
-        court support. They can also refer you to legal and other services:
-      </p>
-        </>
-    );
+export default function StrategicPlans () {
+    const body = <div className="bg-starick-olive pl-8 py-2 mt-5 mb-8">
+        <a href="https://www.starick.org.au/wp-content/uploads/2020-2025-Starick-Strategic-Plan.pdf">Starick Strategic Plan 2021-2025</a>
+    </div>
     return (
         <ServicesSection
           titleTextColour="starick-white"
