e6ecafd89df90b005843f65a685eb4703417a5b2
James Lee <jameslee6352@gmail.com>
2024-01-20 03:08:33 +0800

    Replaced default error component

--- src/pages/404.tsx
+++
@@ -1,8 +0,0 @@
-export default function custom404() {
-  return (
-    <div className="flex flex-grow flex-row items-center justify-center gap-2 text-starick-brown">
-      <h1>Error 404:</h1>
-      <p>Page could not be found</p>
-    </div>
-  );
-}
--- src/pages/500.tsx
+++
@@ -1,8 +0,0 @@
-export default function custom500() {
-  return (
-    <div className="flex h-full flex-row content-center justify-center gap-2 p-24 text-starick-brown">
-      <h1>Error 500:</h1>
-      <p>Internal server error</p>
-    </div>
-  );
-}
