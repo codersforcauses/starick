9d327784dae4f443597c8127595bd7fc9b5bc9e3
lauren <lhpudney@gmail.com>
2024-01-19 23:28:36 +0800

    add heading and body section

---
+++ src/pages/our-people.tsx
@@ -0,0 +1,19 @@
+import SectionTitle from "@/components/section-title"
+export default function OurPeople(){
+    return(
+        <>
+        <div className="bg-starick-white px-6 py-2 md:px-32">
+            <SectionTitle
+            backgroundColour="starick-green"
+            text="Management and staff"
+            textColour="starick-white"
+            />
+            <div className="pt-5 md:px-20">
+                <p>body here</p>
+            </div>
+
+        </div>
+            </>
+    ) 
+    
+}
