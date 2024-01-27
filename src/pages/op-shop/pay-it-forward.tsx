b7c2f12b05ad3520e78970e37c27145933606b67
John Smith <makon1234@users.noreply.github.com>
2024-01-27 12:59:38 +0800

    pay it forward page

--- src/pages/op-shop/pay-it-forward.tsx
+++ src/pages/op-shop/pay-it-forward.tsx
@@ -1,5 +1,11 @@
 import Banner from "@/components/Services/banner";
+import Payitforward from "@/components/OpShop/pay_it_forward";
 
 export default function PayItForward() {
-    return <Banner titleText="Pay It Forward"/>
+    return (
+        <>
+            <Banner titleText="Pay It Forward"/>
+            <Payitforward/> 
+        </>
+            );
 }
