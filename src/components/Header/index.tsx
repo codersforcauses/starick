08cc4285f7796a6564830cbf8e77a850e1ad604f
James Lee <jameslee6352@gmail.com>
2024-01-27 14:09:52 +0800

    Removing search bar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,27 @@
 import NavBar from "./NavBar";
-import SearchBar from "./NavBar/SearchBar";
 import PanicButton from "./PanicButton";
 import MessengerChat from "../MessengerChat";
 
 export default function Header() {
   return (
-    <header>
+    <header className="sticky top-0 z-50">
       <nav className="bg-starick-green">
         <div className="flex flex-row justify-between">
           <PanicButton />
           <div className="flex flex-row items-center gap-1 text-starick-white">
-            <SearchBar prompt="" />
+            <button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="16"
+                width="16"
+                viewBox="0 0 512 512"
+              >
+                <path
+                  className="fill-starick-white"
+                  d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6 .1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"
+                />
+              </svg>
+            </button>
             <h1>FAQ</h1>
             <h1>Contact Us</h1>
             <h1>Useful Numbers</h1>
