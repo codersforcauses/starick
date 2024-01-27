eba8abc245bf6bbfc312951669f3c0514ac7552d
James Lee <jameslee6352@gmail.com>
2024-01-27 11:59:19 +0800

    Rearrangement

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,21 @@
 import NavBar from "./NavBar";
 import SearchBar from "./NavBar/SearchBar";
-import MessengerChat from "../MessengerChat";
 import PanicButton from "./PanicButton";
+import MessengerChat from "../MessengerChat";
 
 export default function Header() {
   return (
     <header>
       <nav className="bg-starick-green">
-        <div className="flex flex-row">
+        <div className="flex flex-row justify-between">
           <PanicButton />
-          <SearchBar prompt="" />
+          <div className="flex flex-row items-center gap-1 text-starick-white">
+            <SearchBar prompt="" />
+            <h1>FAQ</h1>
+            <h1>Contact Us</h1>
+            <h1>Useful Numbers</h1>
+            <h1>Volunteer Now</h1>
+          </div>
         </div>
         <NavBar />
       </nav>
