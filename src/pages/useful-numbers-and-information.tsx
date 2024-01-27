253faf53115dc3063ece661f39d13e1e5e90a713
Temper_Fray <91424496+makon1234@users.noreply.github.com>
2024-01-27 13:16:00 +0800

    Issue 72 create useful numbers and information page (#118)
    * created component files
    
    * useful numbers and informaton page created
    
    * crisis section
    
    * legal section
    
    * added support-info component
    
    * fixed prettier issues
    
    * starick services
    
    * changed colour to match figma
    
    * add line breaks in each subsection
    
    * added links
    
    * add link to useful numbers page
    
    * changed link name
    
    * fixed build errors
    
    ---------
    
    Co-authored-by: John Smith <makon1234@users.noreply.github.com>
    Co-authored-by: unknown <davema0522@163.com>

---
+++ src/pages/useful-numbers-and-information.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+
+import InPageNav from "@/components/InPageNav";
+
+import Banner from "@/components/Services/banner";
+import Crisis from "@/components/useful_numbers_and_information/crisis_section";
+import SupportInfo from "@/components/useful_numbers_and_information/support_info";
+import StarickServices from "@/components/useful_numbers_and_information/starick_services";
+import Legal from "@/components/useful_numbers_and_information/legal";
+
+export default function CoreServices() {
+  const navLinkDetails = [
+    {
+      href: "#Crisis",
+      name: "Crisis"
+    },
+    {
+      href: "#Support Info",
+      name: "Support Info"
+    },
+    {
+      href: "#Starick Services",
+      name: "Starick Services"
+    },
+    {
+      href: "#Legal",
+      name: "Legal"
+    }
+  ];
+  return (
+    <>
+      <Banner titleText="Useful numbers and information" />
+      <InPageNav list={navLinkDetails}></InPageNav>
+      <Crisis />
+      <SupportInfo />
+      <StarickServices />
+      <Legal />
+    </>
+  );
+}
