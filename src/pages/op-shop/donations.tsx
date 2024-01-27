d27dbf91d8eb92220e7518bb5adc8a7e2de4bdf4
Huxley Berry <134905354+HuxleyBerry@users.noreply.github.com>
2024-01-27 14:53:34 +0800

    Issue 116 op shop pages (#120)
    * set up op shop pages
    
    * op shop donations page
    
    * pay it forward page
    
    * prettier
    
    * fix linting error
    
    * starick op shop page
    
    * fixed margins
    
    * run prettier again :(
    
    ---------
    
    Co-authored-by: John Smith <makon1234@users.noreply.github.com>
    Co-authored-by: unknown <davema0522@163.com>

---
+++ src/pages/op-shop/donations.tsx
@@ -0,0 +1,13 @@
+import Banner from "@/components/Services/banner";
+import WhatWeAccept from "@/components/OpShop/what-we-accept";
+import SpecialDonations from "@/components/OpShop/special-donations";
+
+export default function OpShopDonatins() {
+  return (
+    <>
+      <Banner titleText="Op Shop Donations" />
+      <WhatWeAccept />
+      <SpecialDonations />
+    </>
+  );
+}
