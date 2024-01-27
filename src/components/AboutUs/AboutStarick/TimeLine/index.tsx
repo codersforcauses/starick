0f29b8c734e2831a8ed49fe91d8608e2e4432a12
Amy Neve <ladyames20203@gmail.com>
2024-01-27 12:54:39 +0800

    new design to match new figma

--- src/components/AboutUs/AboutStarick/TimeLine/index.tsx
+++ src/components/AboutUs/AboutStarick/TimeLine/index.tsx
@@ -1,44 +1,25 @@
 export default function Timeline() {
   return (
     <div className="container bg-starick-white">
-      <div className="flex grid-cols-9 flex-col p-2 text-starick-black md:grid md:px-32">
-        {/* left */}
-        <div className="flex flex-row-reverse md:contents">
-          {/* text */}
-          <div className="col-start-2 col-end-5 my-4 ml-auto text-right">
-            <h3 className="mb-2 inline-block rounded-lg bg-starick-orange px-2 py-1 text-lg font-semibold text-starick-white">
-              1985
-            </h3>
-            <p className="leading-tight">Official opening of Starick House</p>
-          </div>
-          {/* line */}
-          <div className="relative col-start-5 col-end-6 mr-10 md:mx-auto">
-            <div className="flex h-full w-24 items-center justify-center">
-              <div className="h-full w-1 bg-starick-olive"></div>
-              <div className="absolute top-6 h-4 w-4 rounded-full bg-starick-orange"></div>
-            </div>
-          </div>
+      <div className="mx-auto w-1/4 pt-5">
+        {/* bubble and date */}
+        <div className="mx-auto w-5/6 rounded-lg p-2 text-center shadow-lg">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud
+          </p>
+        </div>
+        <div className="my-3 flex justify-center">
+          <h3 className="text-md inline-block bg-starick-olive px-1 font-semibold">
+            1987
+          </h3>
         </div>
 
-        {/* right */}
-        <div className="flex md:contents">
-          {/* line */}
-          <div className="relative col-start-5 col-end-6 mr-10 md:mx-auto">
-            <div className="flex h-full w-24 items-center justify-center">
-              <div className="h-full w-1 bg-starick-olive"></div>
-              <div className="absolute top-6 h-4 w-4 rounded-full bg-starick-orange"></div>
-            </div>
-          </div>
-          {/* text */}
-          <div className="col-start-6 col-end-9 my-4 mr-auto text-left">
-            <h3 className="mb-2 inline-block rounded-lg bg-starick-orange px-2 py-1 text-lg font-semibold text-starick-white">
-              1987
-            </h3>
-            <p className="leading-tight">
-              Joint production with Curtin University of 30 minutes video to
-              increase awareness of domestic violence (DV) in the community
-            </p>
-          </div>
+        {/* line and dot */}
+        <div className="my-5 flex justify-center">
+          <div className="absolute -my-2 h-6 w-6 rounded-full bg-starick-orange"></div>
+          <div className="h-2 w-full bg-starick-olive"></div>
         </div>
       </div>
     </div>
