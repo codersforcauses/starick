4fd49b69a22cbbfe679c15255c45ea07998d7651
Amy Neve <ladyames20203@gmail.com>
2024-01-31 12:54:57 +0800

    fixed height for desktop

--- src/components/AboutUs/AboutStarick/TimeLine/index.tsx
+++ src/components/AboutUs/AboutStarick/TimeLine/index.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Image from "next/image";
 
 import timelineList from "./timeline-list.json";
-// todo fixed height, scroll transition, progress bar, mobile styling
+// todo scroll transition, progress bar, mobile styling
 import TimelineSection from "./timeline-section";
 export default function Timeline() {
   //range is number of timeline sections shown on screen at once
@@ -10,7 +10,7 @@ export default function Timeline() {
   const [current, setCurrent] = useState(0);
 
   return (
-    <div className="mt-5 flex items-center justify-around gap-0 bg-starick-white px-6 md:px-32">
+    <div className="my-5 flex h-[40vh] items-center justify-around gap-0 bg-starick-white px-6 md:px-32">
       <Image
         src="icons/left-arrow.svg"
         className="mx-2 mb-4 h-6 w-6 opacity-40 hover:opacity-60 md:h-12 md:w-12"
@@ -22,7 +22,7 @@ export default function Timeline() {
       {timelineList.map((section, index) => (
         <div
           key={index}
-          className={`${
+          className={`flex h-full w-1/3 items-end ${
             index >= current && index < current + range ? "" : "hidden"
           }`}
         >
