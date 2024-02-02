e6d697f0acc8d37057e7cd887f0375e524ba8368
James Lee <jameslee6352@gmail.com>
2024-02-03 02:09:48 +0800

    Removed unnecessary margin

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,9 +8,9 @@ import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className=" flex min-h-screen flex-col">
+    <div className="flex min-h-screen flex-col">
       <Header />
-      <main className="mt-14 flex flex-grow flex-col bg-starick-white">
+      <main className="flex flex-grow flex-col bg-starick-white">
         <Assistant />
         <Component {...pageProps} />
       </main>
