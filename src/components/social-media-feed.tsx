9a865f3b292106e6ed30c57f629b775abca08db8
Julie-Salazar <salazarfrancojuliana@gmail.com>
2024-02-07 18:31:33 +0800

    implemented changes regarding insta button and deleted comments

--- src/components/social-media-feed.tsx
+++ src/components/social-media-feed.tsx
@@ -2,21 +2,6 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 
 export default function FacebookFeed() {
-  /*const [facebookData, setFacebookData] = useState(null);
-    useEffect(() => {
-      const fetchData = async () => {
-        const data = await fetch("/api/facebook-feed", {
-          method: "GET"
-        }).then(res => res.json());
-        setFacebookData(data);
-      }
-      fetchData();
-    }, []);
-    if (facebookData) {
-      return (<p>{facebookData}</p>);
-    } else {
-      return null;
-    }*/
   return (
     <div>
       <iframe
@@ -24,17 +9,29 @@ export default function FacebookFeed() {
         width="340"
         height="500"
         title="facebook-embed"
+        className="mx-40 flex justify-center"
       ></iframe>
-      <div className="flex flex-row items-center justify-center">
-        <a href="https://www.instagram.com/starickservices/">
+
+      <div className="mt-20 items-center text-3xl font-bold text-starick-brown ">
+        <p>Get involved! Follow our events on Instagram</p>
+      </div>
+
+      <div className="mt-10 flex flex-row items-center justify-center">
+        <a
+          href="https://www.instagram.com/starickservices/"
+          className="flex w-auto items-center rounded-md bg-blue-500 px-3 py-1 text-white transition-colors duration-300 hover:bg-blue-600"
+        >
           <Image
             src="/icons/instagram.svg"
             alt="instagram"
-            width={40}
+            width={30}
             height={40}
+            className="my-1"
           />
+          <div className=" mx-20">
+            <p>Instagram</p>
+          </div>
         </a>
-        <p className="ml-2">Instagram</p>
       </div>
     </div>
   );
