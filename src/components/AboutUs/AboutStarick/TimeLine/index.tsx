b8a36000fe43980be5977beb3ffaae2d4d536455
Amy Neve <ladyames20203@gmail.com>
2024-02-07 15:27:32 +0800

    progress bar

--- src/components/AboutUs/AboutStarick/TimeLine/index.tsx
+++ src/components/AboutUs/AboutStarick/TimeLine/index.tsx
@@ -10,19 +10,20 @@ export default function Timeline() {
   const [current, setCurrent] = useState(0);
 
   return (
-    <div className="relative my-5 flex h-[40vh] items-center justify-around gap-0 overflow-hidden bg-starick-white px-6 md:px-32">
-      <Image
-        src="icons/left-arrow.svg"
-        className="z-10 mx-2 mb-4 h-6 w-6 opacity-40 hover:opacity-60 md:h-12 md:w-12"
-        height={100}
-        width={100}
-        alt="left-arrow"
-        onClick={() => setCurrent(current - range >= 0 ? current - range : 0)}
-      />
-      {timelineList.map((section, index) => (
-        <div
-          key={index}
-          className={`flex h-full w-1/3 items-end opacity-0
+    <>
+      <div className="relative my-5 flex h-[40vh] items-center justify-around gap-0 overflow-hidden bg-starick-white px-6 md:px-32">
+        <Image
+          src="icons/left-arrow.svg"
+          className="z-10 mx-2 mb-4 h-6 w-6 opacity-40 hover:opacity-60 md:h-12 md:w-12"
+          height={100}
+          width={100}
+          alt="left-arrow"
+          onClick={() => setCurrent(current - range >= 0 ? current - range : 0)}
+        />
+        {timelineList.map((section, index) => (
+          <div
+            key={index}
+            className={`flex h-full w-1/3 items-end opacity-0
           ${
             index >= current && index < current + range
               ? "translate-x-0 transform opacity-100 transition duration-500 ease-in-out"
@@ -31,27 +32,40 @@ export default function Timeline() {
           ${index >= current + range ? "translate-x-full" : ""}
           ${index < current ? "-translate-x-full" : ""}
           `}
-        >
-          <TimelineSection
-            year={section.year}
-            description={section.description}
-          />
-        </div>
-      ))}
-      <Image
-        src="icons/right-arrow.svg"
-        className="mx-2 mb-4 h-6 w-6 opacity-40 hover:opacity-60 md:h-12 md:w-12"
-        height={100}
-        width={100}
-        alt="right-arrow"
-        onClick={() =>
-          setCurrent(
-            current + range <= timelineList.length - 3
-              ? current + range
-              : timelineList.length - 3
-          )
-        }
-      />
-    </div>
+          >
+            <TimelineSection
+              year={section.year}
+              description={section.description}
+            />
+          </div>
+        ))}
+        <Image
+          src="icons/right-arrow.svg"
+          className="mx-2 mb-4 h-6 w-6 opacity-40 hover:opacity-60 md:h-12 md:w-12"
+          height={100}
+          width={100}
+          alt="right-arrow"
+          onClick={() =>
+            setCurrent(
+              current + range <= timelineList.length - 3
+                ? current + range
+                : timelineList.length - 3
+            )
+          }
+        />
+      </div>
+      <div className="my-5 flex justify-center">
+        {Array.from({ length: timelineList.length / 3 + 1 }).map((_, index) => (
+          <div
+            key={index}
+            className={`-my-2 mx-0.5 h-4 w-4 rounded-full ${
+              index == Math.floor((current + 1) / range)
+                ? "bg-starick-green"
+                : "bg-starick-olive"
+            }`}
+          ></div>
+        ))}
+      </div>
+    </>
   );
 }
