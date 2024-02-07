1c5aa2366aa8dab3b7ea26628055ee3dc532921b
Amy Neve <ladyames20203@gmail.com>
2024-02-07 14:32:17 +0800

    transition kinda? can only get it to work coming in not out

--- src/components/AboutUs/AboutStarick/TimeLine/index.tsx
+++ src/components/AboutUs/AboutStarick/TimeLine/index.tsx
@@ -5,15 +5,15 @@ import timelineList from "./timeline-list.json";
 // todo scroll transition, progress bar, mobile styling
 import TimelineSection from "./timeline-section";
 export default function Timeline() {
-  //range is number of timeline sections shown on screen at once
-  const range = 3; //todo adjust this number to change for smaller screens etc
+  // range is number of timeline sections shown on screen at once
+  const range = 3; // todo adjust this number to change for smaller screens etc
   const [current, setCurrent] = useState(0);
 
   return (
-    <div className="my-5 flex h-[40vh] items-center justify-around gap-0 bg-starick-white px-6 md:px-32">
+    <div className="relative my-5 flex h-[40vh] items-center justify-around gap-0 overflow-hidden bg-starick-white px-6 md:px-32">
       <Image
         src="icons/left-arrow.svg"
-        className="mx-2 mb-4 h-6 w-6 opacity-40 hover:opacity-60 md:h-12 md:w-12"
+        className="z-10 mx-2 mb-4 h-6 w-6 opacity-40 hover:opacity-60 md:h-12 md:w-12"
         height={100}
         width={100}
         alt="left-arrow"
@@ -22,9 +22,15 @@ export default function Timeline() {
       {timelineList.map((section, index) => (
         <div
           key={index}
-          className={`flex h-full w-1/3 items-end ${
-            index >= current && index < current + range ? "" : "hidden"
-          }`}
+          className={`flex h-full w-1/3 items-end opacity-0
+          ${
+            index >= current && index < current + range
+              ? "translate-x-0 transform opacity-100 transition duration-500 ease-in-out"
+              : "absolute"
+          }
+          ${index >= current + range ? "translate-x-full" : ""}
+          ${index < current ? "-translate-x-full" : ""}
+          `}
         >
           <TimelineSection
             year={section.year}
