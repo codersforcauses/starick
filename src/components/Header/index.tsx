30413f2bc05d7532e107100bb576beab1859f4de
James Lee <jameslee6352@gmail.com>
2024-02-13 01:25:52 +0800

    minor tweak

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -31,7 +31,7 @@ export default function Header() {
         role="search"
         className={`${
           searchOpen ? "" : "hidden"
-        } fixed bottom-0 left-0 right-0 top-0 flex h-full items-center justify-around bg-starick-green`}
+        } fixed bottom-0 left-0 right-0 top-0 flex items-center justify-around bg-starick-green`}
       >
         <input
           placeholder="Search..."
