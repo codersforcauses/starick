2e67761477237a1050f3f6f4c237541d70f727fb
LAURENNNNNN!!!! <lhpudney@gmail.com>
2024-02-17 17:44:43 +0800

    add our-board, management-and-staff and portrait sections

---
+++ src/components/OurPeople/management-and-staff.tsx
@@ -0,0 +1,11 @@
+import Portrait from "./portrait";
+
+export default function ManagementAndStaff() {
+    return( 
+    <>
+    <p>Management and Staff</p>
+    <Portrait name="Anne" imagePath="/images/our-people/Anne.jpg" info="blah"/>
+    </>
+
+    );
+}     
