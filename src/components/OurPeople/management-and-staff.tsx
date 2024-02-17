cf90f3eb335a887a53f71b833adb3a2d3c8da925
= <lhpudney@gmail.com>
2024-02-17 20:11:47 +0800

    add portraits for management and staff

--- src/components/OurPeople/management-and-staff.tsx
+++ src/components/OurPeople/management-and-staff.tsx
@@ -1,11 +1,75 @@
 import Portrait from "./portrait";
 
 export default function ManagementAndStaff() {
-    return( 
+  return (
     <>
-    <p>Management and Staff</p>
-    <Portrait name="Anne" imagePath="/images/our-people/Anne.jpg" info="blah"/>
+      <div className="pt-5 md:px-20">
+        <p className="px-16 py-16 text-center text-2xl font-semibold">
+          Management and Staff
+        </p>
+        <div className="mx-2.5 my-2.5 flex flex-wrap justify-center gap-8">
+          <Portrait
+            name="Leanne"
+            position="Chief Executive Officer"
+            imagePath="/images/our-people/Leanne.jpg"
+            info="blah"
+          />
+          <Portrait
+            name="Francine"
+            position="Chief Financial Officer"
+            imagePath="/images/our-people/Francine.jpg"
+            info="blah"
+          />
+          <Portrait
+            name="Kathleen"
+            position="Senior Social Worker"
+            imagePath="/images/our-people/Kathleen.jpg"
+            info="blah"
+          />
+          <Portrait
+            name="Caroline"
+            position="Outreach Coordinator"
+            imagePath="/images/our-people/Caroline.png"
+            info="blah"
+          />
+          <Portrait
+            name="Jodie"
+            position="Outreach Worker"
+            imagePath="/images/our-people/Jodie.png"
+            info="blah"
+          />
+          <Portrait
+            name="Anne"
+            position="Domestic Violence Advocate"
+            imagePath="/images/our-people/Anne.jpg"
+            info="blah"
+          />
+          <Portrait
+            name="Nadia"
+            position="Domestic Violence Advocate"
+            imagePath="/images/our-people/Nadia.jpg"
+            info="blah"
+          />
+          <Portrait
+            name="Elena"
+            position="Women's Advocate"
+            imagePath="/images/our-people/Elena.jpg"
+            info="blah"
+          />
+                    <Portrait
+            name="Fei"
+            position="Children's Advocate"
+            imagePath="/images/our-people/Fei.jpg"
+            info="blah"
+          />
+                    <Portrait
+            name="Sandie"
+            position="Volunteer Op Shop Manager"
+            imagePath="/images/our-people/Sandie.jpg"
+            info="blah"
+          />
+        </div>
+      </div>
     </>
-
-    );
-}     
+  );
+}
--- src/components/OurPeople/portrait.tsx
+++ src/components/OurPeople/portrait.tsx
@@ -1,22 +1,23 @@
 import Image from "next/image";
 
 interface PortraitInfo {
-    name: string;
-    info: string;
-    imagePath: string;
+  name: string;
+  position: string;
+  info: string;
+  imagePath: string;
 }
 
-export default function Portrait( {
-    name,
-    info,
-    imagePath }: PortraitInfo) {
+export default function Portrait({ name, position, info, imagePath }: PortraitInfo) {
+  return (
+    <>
+      <div>
+        <img src={imagePath} alt={name} className="h-64 w-auto"/>
+        <div className="h-36 bg-starick-green text-starick-white px-4 py-4">
+          <p className="font-semibold text-2xl ">{name}</p>
+          <p className="pt-6">{position}</p>
 
-    return(
-        <>
-        <p>{name}</p>
-        <Image src = {imagePath} alt={name} width={200} height={200}/>
-
-        </>
-    )
-
+        </div>
+      </div>
+    </>
+  );
 }
