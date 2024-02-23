cbd6373fa63b27260ceb42f2d37e1ba93ff74aa2
Katrina Yan <katrina.yan1707@gmail.com>
2024-02-24 03:19:52 +0800

    domestic violence page to be tweaked, FAQ page not done yet.

---
+++ src/components/DomesticViolence/recognising-the-signs.tsx
@@ -0,0 +1,53 @@
+import SubSection from "../Services/subsection";
+import ServicesSection from "../Services/services-section";
+import { List } from "../Services/list";
+
+export default function RecognisingTheSignsSection() {
+  const domesticViolenceSignsExample = (
+    <List
+      items={[
+        "Do you have to be careful around this person because of their anger, violence or emotional abuse?",
+        "Are you pressured by this person into sexual activities you are not comfortable with?",
+        "Are you are too scared to disagree with this person?",
+        "Is this person always checking up on what you are doing?",
+        "Does this person say you are stupid or useless?",
+        "Does this person threaten you, your family or your pets?",
+        "Does this person stop you going out or doing what you want to do?",
+        "Are you unable to spend time with family or friends because this person won’t let you?",
+        "Do you make excuses for this person’s violent behaviour?"
+      ]}
+      style="list-disc"
+    />
+  );
+  const subBody = (
+    <div className="pl-3">
+      <p className="text-xl font-semibold">
+        Is there someone in your life who is behaving in some or all of the
+        following ways?
+      </p>
+      {domesticViolenceSignsExample}
+      <br />
+      <p>If so, you may want to seek assistance or contact a Starick worker to discuss your situation</p>
+    </div>
+  );
+
+  const body = (
+    <>
+      <SubSection
+        sectionBody={subBody}
+        iconPath="icons/exclamation.svg"
+        iconOnRight={false}
+      />
+    </>
+  );
+  return (
+    <ServicesSection
+      titleTextColour="starick-white"
+      titleBackgroundColour="starick-green"
+      sectionBody={body}
+      title="Recognising the signs"
+      textOnLeft={true}
+      circlesPlacement="br"
+    />
+  );
+}
