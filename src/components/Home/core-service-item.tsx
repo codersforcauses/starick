57c3709b7dfb93862fcd975288e8ee4972c72d02
Jules :) <92288226+Julie-Salazar@users.noreply.github.com>
2024-02-26 09:58:15 +0800

    Issue 40 home page facebook plugin (#141)
    * trying simple facebook embed method
    
    * center facebook feed and add instagram link
    
    * deleted banner and added crisis alert svg
    
    * added basic grid component with the core services and other services, need direct link buttons
    
    * skeleton for sequence of elememts in homepage according to figma and personal op
    
    * implemented changes regarding insta button and deleted comments
    
    * implemented changes regarding insta button and deleted comments
    
    * implemented changes regarding insta button and deleted comments
    
    * moved events to past event carousel, moved intagram button and matched style, added picture to opshop
    
    * initial commit for did you know/Library/Developer/CommandLineTools/usr/bin/python3 -m pip install --upgrade pip
    
    * replaced images with starick images, putting insta and fb together in one place, changing the links to other services and formatting to change icons in next commit as well as link to correct pages
    
    * replaced images with starick images, putting insta and fb together in one place, changing the links to other services and formatting to change icons in next commit as well as link to correct pages
    
    * need help
    
    * stashed changes
    
    * changed the links to the to core services and the other services as well as scaled images
    
    ---------
    
    Co-authored-by: HuxleyBerry <huxley.berry@gmail.com>

--- src/components/Home/core-service-item.tsx
+++ src/components/Home/core-service-item.tsx
@@ -5,12 +5,17 @@ import { StaticImageData } from "next/image";
 interface ItemProps {
   img: StaticImageData;
   text: string;
+  iconClassName?: string; // Add the iconClassName prop
 }
 
-export default function CoreServiceItem({ img, text }: ItemProps) {
+export default function CoreServiceItem({
+  img,
+  text,
+  iconClassName
+}: ItemProps) {
   return (
     <div className="flex items-center justify-items-start">
-      <div className="relative h-24 w-24">
+      <div className={`relative h-24 w-24 ${iconClassName}`}>
         <Image src={img} alt="description" layout="fill" objectFit="cover" />
       </div>
       <p className="ml-20">{text}</p>
