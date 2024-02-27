d970867b4b272c1dccb1a04a186edc14c499f20a
K-Straiton <straiton98@gmail.com>
2024-02-27 15:31:21 +0800

    Run prettier and lint

--- src/pages/api/email/email.ts
+++ src/pages/api/email/email.ts
@@ -1,28 +1,30 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'POST') {
-      
-      const details = JSON.parse(req.body)
-      
-      const formBody = [];
-      let newFormBody = ""
-      for (const property in details) {
-          const encodedKey = encodeURIComponent(property);
-          const encodedValue = encodeURIComponent(details[property]);
-          formBody.push(encodedKey + "=" + encodedValue);
-        }
-        newFormBody = formBody.join("&");
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const details = JSON.parse(req.body);
 
-        const url = process.env.NEXT_PUBLIC_FORM_RESPONSE_URL;
-        const response = await fetch(url as string , {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-            },
-            body: newFormBody
-        })
+    const formBody = [];
+    let newFormBody = "";
+    for (const property in details) {
+      const encodedKey = encodeURIComponent(property);
+      const encodedValue = encodeURIComponent(details[property]);
+      formBody.push(encodedKey + "=" + encodedValue);
+    }
+    newFormBody = formBody.join("&");
 
-      res.status(response.status).send({});
+    const url = process.env.NEXT_PUBLIC_FORM_RESPONSE_URL;
+    const response = await fetch(url as string, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+      },
+      body: newFormBody
+    });
+
+    res.status(response.status).send({});
   }
 }
