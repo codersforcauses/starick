cd43bec23100671923a1aa3c56df8870344cd495
Aifert Yet <aifertyet0628@gmail.com>
2024-03-06 17:24:12 +0800

    finished our people

--- src/components/OurPeople/management-and-staff.tsx
+++ src/components/OurPeople/management-and-staff.tsx
@@ -7,67 +7,79 @@ export default function ManagementAndStaff() {
         <p className="px-16 py-16 text-center text-2xl font-semibold">
           Management and Staff
         </p>
-        <div className="mx-2.5 my-2.5 flex flex-wrap justify-center gap-8">
+        <div className="grid grid-cols-3 justify-center items-center">
           <Portrait
             name="Leanne"
             position="Chief Executive Officer"
             imagePath="/images/our-people/Leanne.jpg"
             info="blah"
+            board={false}
           />
           <Portrait
             name="Francine"
             position="Chief Financial Officer"
             imagePath="/images/our-people/Francine.jpg"
             info="blah"
+            board={false}
           />
           <Portrait
             name="Kathleen"
             position="Senior Social Worker"
             imagePath="/images/our-people/Kathleen.jpg"
             info="blah"
+            board={false}
           />
           <Portrait
             name="Caroline"
             position="Outreach Coordinator"
             imagePath="/images/our-people/Caroline.png"
             info="blah"
+            board={false}
           />
           <Portrait
             name="Jodie"
             position="Outreach Worker"
             imagePath="/images/our-people/Jodie.png"
             info="blah"
+            board={false}
           />
           <Portrait
             name="Anne"
             position="Domestic Violence Advocate"
             imagePath="/images/our-people/Anne.jpg"
             info="blah"
+            board={false}
           />
           <Portrait
             name="Nadia"
             position="Domestic Violence Advocate"
             imagePath="/images/our-people/Nadia.jpg"
             info="blah"
+            board={false}
           />
           <Portrait
             name="Elena"
             position="Women's Advocate"
             imagePath="/images/our-people/Elena.jpg"
             info="blah"
+            board={false}
           />
-                    <Portrait
+          <Portrait
             name="Fei"
             position="Children's Advocate"
             imagePath="/images/our-people/Fei.jpg"
             info="blah"
+            board={false}
           />
-                    <Portrait
-            name="Sandie"
-            position="Volunteer Op Shop Manager"
-            imagePath="/images/our-people/Sandie.jpg"
-            info="blah"
-          />
+        </div>
+        <div>
+          <Portrait
+              name="Sandie"
+              position="Volunteer Op Shop Manager"
+              imagePath="/images/our-people/Sandie.jpg"
+              info="last"
+              board={false}
+            />
         </div>
       </div>
     </>
--- src/components/OurPeople/our-board.tsx
+++ src/components/OurPeople/our-board.tsx
@@ -1,55 +1,64 @@
-import Portrait from "./portrait";
+import Board from "./board";
 
 export default function OurBoard() {
   return (
     <>
+    <div className = "mb-10">
       <p className="px-16 py-16 text-center text-2xl font-semibold">
-        Our Board
       </p>
-      <div className="mx-2.5 my-2.5 flex flex-wrap justify-center gap-8">
-        <Portrait
+      <div className="mx-2.5 my-2.5 flex flex-col gap-8 w-auto items-center">
+        <Board
           name="Natalie Sangalli"
           position="Chair"
           imagePath="/images/our-people/natalie-sangalli.jpg"
-          info="blah"
+          info="Natalie is the General Manager Community Housing at Access Housing and on the Board of ShelterWA, delivering social and affordable housing solutions to over 3,000 Western Australians. Prior to this she worked in the Justice Sector in both government and private organisations for over 16 years, advocating social justice issues and built a portfolio of achievements leading strategic service delivery and infrastructure projects, managing high profile, high risk contracts and leading service delivery teams."
+          reverse={false}
         />
-        <Portrait
+        <Board
           name="Gail Brindley"
           position="Deputy Chair"
           imagePath="/images/our-people/gail-brindley.jpg"
-          info="blah"
+          info="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          reverse={true}
         />
-        <Portrait
+        <Board
           name="Liza Dunne"
           position="Treasurer"
           imagePath="/images/our-people/liza-dunne.jpg"
-          info="blah"
+          info="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          reverse={false}
         />
-        <Portrait
+        <Board
           name="Carolyn Turner"
           position="Board Member"
           imagePath="/images/our-people/carolyn-turner.jpg"
-          info="blah"
+          info="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          reverse={true}
         />
-        <Portrait
+        <Board
           name="Kylie Cutten"
           position="Board Member"
           imagePath="/images/our-people/kylie-cutten.png"
-          info="blah"
+          info="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          reverse={false}
         />
-        <Portrait
+        <Board
           name="Claudia Mancini"
           position="Board Member"
           imagePath="/images/our-people/claudia-mancini.png"
-          info="blah"
+          info="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          reverse={true}
         />
-        <Portrait
+        <Board
           name="Anna Wildy"
           position="Board Member"
           imagePath="/images/our-people/anna-wildy.jpg"
-          info="blah"
+          info="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          reverse={false}
         />
-      </div>
+      </div>      
+    </div>
+
     </>
   );
 }
