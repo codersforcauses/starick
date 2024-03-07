c6246910f010bca1f05065509aa29465522c1c0e
Jules :) <92288226+Julie-Salazar@users.noreply.github.com>
2024-03-07 16:44:49 +0800

    Issue 40 home page facebook plugin (#149)
    * trying simple facebook embed method
    
    * center facebook feed and add instagram link
    
    * deleted banner and added crisis alert svg
    
    * added basic grid component with the core services and other services, need direct link buttons
    
    * skeleton for sequence of elememts in homepage according to figma and personal op
    
    * implemented changes regarding insta button and deleted comments
    
    * implemented changes regarding insta button and deleted comments
    
    * implemented changes regarding insta button and deleted comments
    
    * moved events to past event carousel, moved intagram button and matched style, added picture to opshop
    
    * initial commit for did you know/Library/Developer/CommandLineTools/usr/bin/python3 -m pip install --upgrade pip
    
    * replaced images with starick images, putting insta and fb together in one place, changing the links to other services and formatting to change icons in next commit as well as link to correct pages
    
    * replaced images with starick images, putting insta and fb together in one place, changing the links to other services and formatting to change icons in next commit as well as link to correct pages
    
    * need help
    
    * stashed changes
    
    * changed the links to the to core services and the other services as well as scaled images
    
    * changed the centering of social medias as well as make all hyperlinked and buttons to
     change colour when hovered over, having trouble with doing that with the two quick links at the top of the page
    
    * run prettier
    
    * commiting but need to do local pull
    
    * chewing at the bars in my enclosure :(
    
    * fixed <p tag issue and changes remaining service links for button tags
    
    * fixed linking issues, took out nested <p and changed for <spans, followed link guide, please let me know if deployment works on your side, fixed bg trasluscent overlay
    
    * fixed linking issues, took out nested <p and changed for <spans, followed link guide, please let me k
    now if deployment works on your side, fixed bg trasluscent overlay
    
    * fixed transitional housing spacing and the bg-black-50 should be attatched to main image now
    
    ---------
    
    Co-authored-by: HuxleyBerry <huxley.berry@gmail.com>

---
+++ src/components/ChatBot/message.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { GPTMessage } from "@/lib/chatbot/validators";
+
+const styles = {
+  user: "bg-starick-orange text-white self-end",
+  system: "bg-rose-400 self-center",
+  assistant: "bg-messenger-grey text-starick-black"
+};
+
+function Message({ role, content }: GPTMessage) {
+  return (
+    <div className={`${styles[role]} max-w-[80%] rounded-md p-1`}>
+      {content}
+    </div>
+  );
+}
+
+export default Message;
---
+++ src/pages/mynumber.java
@@ -0,0 +1,10 @@
+public class MyNumber@ {
+    pulic static void main(string[] args){
+        String myString = "hi";
+        int mynumber;
+        myNumber = 5*2 + 1;
+        myNumber = 0
+        System.out.println("The number is " + myNumber);
+
+    }
+}
