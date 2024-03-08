857e2ea20683d56dff0eb00884a0ff8dcc36f29a
Aifert Yet <aifertyet0628@gmail.com>
2024-03-08 12:08:20 +0800

    delete banner to match figma for our people page

--- src/pages/people.tsx
+++ src/pages/people.tsx
@@ -6,7 +6,6 @@ import Banner from "@/components/Services/banner";
 export default function OurPeople() {
   return (
     <>
-      <Banner titleText="Our People" />
       <PageHeader titleText="About Starick" />
       <ManagementAndStaff />
       <OurBoard />
