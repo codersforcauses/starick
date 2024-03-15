80e235d9c660a145a87d2cd48107791fd831b8aa
Aifert Yet <aifertyet0628@gmail.com>
2024-03-15 08:57:20 +0800

    fixed dynamic positioning of navbar

--- src/components/Header/NavBar/index.tsx
+++ src/components/Header/NavBar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -6,14 +6,32 @@ import { primaryNav } from "./links";
 import NavItem from "./nav-item";
 
 export default function NavBar() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   // handles hamburger button state
   const [hamburgerMenuOpen, setHamburgerMenuOpen] = useState(false);
 
   return (
-    <div className="flex h-20 items-center justify-center whitespace-nowrap bg-white px-4 text-starick-black">
+    <div className="flex h-20 items-center justify-center gap-60 whitespace-nowrap bg-white px-4 text-starick-black">
       {/* Left nav container (logo and links) */}
       {/* Header container */}
-      <div className="flex flex-row items-center gap-80">
+      <div
+        className={`flex flex-row items-center ${
+          windowWidth >= 1200 ? "lg:gap-60" : "md:gap-30"
+        }`}
+      >
         <Link href="/">
           <Image
             src="/placeholder_starick_logo.png"
